6f259e3cd1c87c3ecfcfa79d79fe16b9f34d3f10
IzaakWN <iwn_@hotmail.com>
2020-07-03 18:00:53 +0200

    automatically add __init_.py for new analysis module

--- common/python/tools/file.py
+++ common/python/tools/file.py
@@ -90,6 +90,18 @@ def ensureTFile(filename,option='READ'):
   return file
   
 
+def ensureinit(*paths,**kwargs):
+  """Ensure an __init__.py exists, other wise, create one."""
+  init   = os.path.join(os.path.join(*paths),'__init__.py')
+  script = kwargs.get('by',"")
+  if not os.path.isfile(init):
+    print ">>> Creating '%s' to allow import of module..."%(init)
+    with open(init,'w') as file:
+      if script:
+        script = "by "+script
+      file.write("# Generated%s to allow import of the sample list modules\n"%(script))
+  
+
 #def extractTH1(file,histname,setdir=True,close=None):
 #  """Get histogram from a given file."""
 #  if isinstance(file,str):
