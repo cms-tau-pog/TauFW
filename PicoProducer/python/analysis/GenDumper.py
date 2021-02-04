649c1c45eb4bd30413fd860d0601eae1bd5b6fa1
IzaakWN <iwn_@hotmail.com>
2021-02-04 15:40:15 +0100

    add decay chain print out

--- PicoProducer/python/analysis/GenDumper.py
+++ PicoProducer/python/analysis/GenDumper.py
@@ -10,20 +10,41 @@ from PhysicsTools.NanoAODTools.postprocessing.framework.eventloop import Module
 from PhysicsTools.NanoAODTools.postprocessing.framework.datamodel import Collection
 from TauFW.PicoProducer.analysis.utils import hasbit
 
+
+def parsechain(particles,seeds,chain,indent=""):
+  """Recursively print decay chain from dictionary."""
+  string = ""
+  for i, seed in enumerate(seeds):
+    if indent=="": # beginning of chain
+      substr = "%3d(%d,%d)"%(particles[seed].pdgId,seed,particles[seed].status)
+    else:
+      substr = " -> %3d(%d,%d)"%(particles[seed].pdgId,seed,particles[seed].status)
+    if len(chain[seed])>0:
+      subind  = indent+' '*len(substr)
+      substr += parsechain(particles,chain[seed],chain,subind)
+    string += substr
+    if i<len(seeds)-1:
+      string += '\n'+indent
+  return string
+
+
 # DUMPER MODULE
 class GenDumper(Module):
   
   def __init__(self,*args,**kwargs):
     self.nleptons = 0
     self.nevents  = 0
+    self.seedpids = [23] # print decay chain of particles with these PIDs
   
   def analyze(self,event):
     """Dump gen information for each gen particle in given event."""
-    print "\n%s event %s %s"%('-'*10,event.event,'-'*60)
+    print "\n%s event %s %s"%('-'*10,event.event,'-'*68)
     self.nevents += 1
-    leptonic = False
+    leptonic  = False
     particles = Collection(event,'GenPart')
     #particles = Collection(event,'LHEPart')
+    seeds     = [ ] # seeds for decay chain
+    chain     = { } # decay chain
     print " \033[4m%7s %8s %8s %8s %8s %8s %8s %8s %9s %10s  \033[0m"%(
       "index","pdgId","moth","mothid","dR","pt","eta","status","prompt","last copy")
     for i, particle in enumerate(particles):
@@ -31,19 +52,26 @@ class GenDumper(Module):
       if 0<=mothidx<len(particles):
         moth    = particles[mothidx]
         mothpid = moth.pdgId
-        mothdR  = min(999,particle.DeltaR(moth)) #particle.p4().DeltaR(moth.p4())
+        mothdR  = min(9999,particle.DeltaR(moth)) #particle.p4().DeltaR(moth.p4())
       else:
         mothpid = -1
         mothdR  = -1
-      eta       = min(999,particle.eta)
+      eta       = max(-9999,min(9999,particle.eta))
       prompt    = hasbit(particle.statusFlags,0)
       lastcopy  = hasbit(particle.statusFlags,13)
       print " %7d %8d %8d %8d %8.2f %8.2f %8.2f %8d %9s %10s"%(
         i,particle.pdgId,mothidx,mothpid,mothdR,particle.pt,eta,particle.status,prompt,lastcopy)
       if abs(particle.pdgId) in [11,13,15]:
         leptonic = True
+      if mothidx in chain: # add to decay chain
+        chain[mothidx].append(i)
+        chain[i] = [ ] # daughters
+      elif abs(particle.pdgId) in self.seedpids: # save as decay chain seed
+        seeds.append(i)
+        chain[i] = [ ] # daughters
     if leptonic:
       self.nleptons += 1
+    print parsechain(particles,seeds,chain) # print decay chain
   
   def endJob(self):
     print '\n'+'-'*80
