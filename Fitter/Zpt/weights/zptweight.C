7a847bd517669dd28678fa3229280f5b744155dc
IzaakWN <iwn_@hotmail.com>
2021-06-02 10:28:11 +0200

    add Z pt weight macro

---
+++ Fitter/Zpt/weights/zptweight.C
@@ -0,0 +1,65 @@
+/* @short: provide Z pt weights at drawing level for checks
+ * @author: Izaak Neutelings (June 2018)
+ * @comment: in python:
+ *   gROOT.Macro('weights/zptweight.C+')
+ *   from ROOT import loadZptWeights
+ *   loadZptWeights("weights.root","zpt_weight")
+ *   tree.Draw("pt_ll >> h","getZptWeight(pt_moth)",'gOff')
+ */
+#include "TROOT.h"
+#include "TFile.h"
+#include "TH1.h"
+//#include "TH2.h"
+//#include "TH1F.h"
+//#include "TH2F.h"
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+TString _fname = "weights/zpt_weight_$ERA$TAG.root";
+//TString _fname = "$CMSSW_BASE/src/TauFWFitter/Zpt/weights/$ERA/zpt_weight_$ERA$TAG.root";
+TString _hname = "zptweight";
+TH1* histZpt;
+//TH2F* histZpt;
+
+void loadZptWeights(TString fname="2017", TString hname=_hname, TString tag=""){
+  if(!fname.EndsWith(".root"))
+    fname = _fname.Copy().ReplaceAll("$ERA",fname).ReplaceAll("$TAG",tag);
+  std::cout << ">>> loadZptWeights(): opening " << fname << ":" << hname << std::endl;
+  if(histZpt)
+    histZpt->Delete();
+  TFile *file = new TFile(fname);
+  histZpt = (TH1*) file->Get(hname);
+  if(!histZpt){
+    std::cerr << ">>> loadZptWeights(): Did not find " << hname << " in " << fname << std::endl;
+    file->ls();
+  }
+  histZpt->SetDirectory(0);
+  file->Close();
+}
+
+Float_t getZptWeight(Float_t pt){
+  Int_t xbin = histZpt->GetXaxis()->FindBin(pt);
+  Float_t weight = 1.0;
+  while(xbin<1) xbin++;
+  while(xbin>histZpt->GetXaxis()->GetNbins()) xbin--;
+  weight = histZpt->GetBinContent(xbin);
+  if(weight<=0.0) return 1.;
+  return histZpt->GetBinContent(xbin);
+}
+
+Float_t getZptWeight2D(Float_t pt, Float_t mass){
+  Int_t xbin = histZpt->GetXaxis()->FindBin(pt);
+  Int_t ybin = histZpt->GetYaxis()->FindBin(mass);
+  Float_t weight = 1.0;
+  while(xbin<1) xbin++; while(xbin>histZpt->GetXaxis()->GetNbins()) xbin--;
+  while(ybin<1) ybin++; while(ybin>histZpt->GetYaxis()->GetNbins()) ybin--;
+  weight = histZpt->GetBinContent(xbin,ybin);
+  if(weight<=0.0) return 1.;
+  return histZpt->GetBinContent(xbin,ybin);
+}
+
+void zptweight(){
+  std::cout << ">>> initializing zptweight.C ... " << std::endl;
+}
+
