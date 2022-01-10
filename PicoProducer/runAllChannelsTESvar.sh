083a1954080dd6e386ff7ff38abce84086ce3aa9
Saskia Falke <saskia.falke@cern.ch>
2022-01-10 12:27:37 +0100

    Changes to run samples for tes fit

---
+++ PicoProducer/runAllChannelsTESvar.sh
@@ -0,0 +1,35 @@
+
+
+job=$1 # can be: run, submit, resubmit, hadd, ...
+
+pico.py $job -y UL2018 -c mutau_TES0p970 -s DY TT --das
+pico.py $job -y UL2018 -c mutau_TES0p972 -s DY TT --das
+pico.py $job -y UL2018 -c mutau_TES0p974 -s DY TT --das
+pico.py $job -y UL2018 -c mutau_TES0p976 -s DY TT --das
+pico.py $job -y UL2018 -c mutau_TES0p978 -s DY TT --das
+pico.py $job -y UL2018 -c mutau_TES0p980 -s DY TT --das
+pico.py $job -y UL2018 -c mutau_TES0p982 -s DY TT --das
+pico.py $job -y UL2018 -c mutau_TES0p984 -s DY TT --das
+pico.py $job -y UL2018 -c mutau_TES0p986 -s DY TT --das
+pico.py $job -y UL2018 -c mutau_TES0p988 -s DY TT --das
+pico.py $job -y UL2018 -c mutau_TES0p990 -s DY TT --das
+pico.py $job -y UL2018 -c mutau_TES0p992 -s DY TT --das
+pico.py $job -y UL2018 -c mutau_TES0p994 -s DY TT --das
+pico.py $job -y UL2018 -c mutau_TES0p996 -s DY TT --das
+pico.py $job -y UL2018 -c mutau_TES0p998 -s DY TT --das
+pico.py $job -y UL2018 -c mutau_TES1p000 -s DY TT --das
+pico.py $job -y UL2018 -c mutau_TES1p002 -s DY TT --das
+pico.py $job -y UL2018 -c mutau_TES1p004 -s DY TT --das
+pico.py $job -y UL2018 -c mutau_TES1p006 -s DY TT --das
+pico.py $job -y UL2018 -c mutau_TES1p008 -s DY TT --das
+pico.py $job -y UL2018 -c mutau_TES1p010 -s DY TT --das
+pico.py $job -y UL2018 -c mutau_TES1p012 -s DY TT --das
+pico.py $job -y UL2018 -c mutau_TES1p014 -s DY TT --das
+pico.py $job -y UL2018 -c mutau_TES1p016 -s DY TT --das
+pico.py $job -y UL2018 -c mutau_TES1p018 -s DY TT --das
+pico.py $job -y UL2018 -c mutau_TES1p020 -s DY TT --das
+pico.py $job -y UL2018 -c mutau_TES1p022 -s DY TT --das
+pico.py $job -y UL2018 -c mutau_TES1p024 -s DY TT --das
+pico.py $job -y UL2018 -c mutau_TES1p026 -s DY TT --das
+pico.py $job -y UL2018 -c mutau_TES1p028 -s DY TT --das
+pico.py $job -y UL2018 -c mutau_TES1p030 -s DY TT --das
