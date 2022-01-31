211dec8f9e35f0cfc65a62c905f8d3233862317a
Saskia Falke <saskia.falke@cern.ch>
2022-01-31 09:22:59 +0100

    Intermediate save before merging changes from Izaak

--- PicoProducer/runAllChannelsTESvar.sh
+++ PicoProducer/runAllChannelsTESvar.sh
@@ -1,39 +1,39 @@
 job=$1 # can be: run, submit, resubmit, hadd, ...
 
-#pico.py $job -y UL2018 -c mutau_TES0p970 -s DY --das
-#pico.py $job -y UL2018 -c mutau_TES0p972 -s DY --das
-#pico.py $job -y UL2018 -c mutau_TES0p974 -s DY --das
-#pico.py $job -y UL2018 -c mutau_TES0p976 -s DY --das
-#pico.py $job -y UL2018 -c mutau_TES0p978 -s DY --das
-#pico.py $job -y UL2018 -c mutau_TES0p980 -s DY --das
-#pico.py $job -y UL2018 -c mutau_TES0p982 -s DY --das
-#pico.py $job -y UL2018 -c mutau_TES0p984 -s DY --das
-#pico.py $job -y UL2018 -c mutau_TES0p986 -s DY --das
-#pico.py $job -y UL2018 -c mutau_TES0p988 -s DY --das
-#pico.py $job -y UL2018 -c mutau_TES0p990 -s DY --das
-#pico.py $job -y UL2018 -c mutau_TES0p992 -s DY --das
-#pico.py $job -y UL2018 -c mutau_TES0p994 -s DY --das
-#pico.py $job -y UL2018 -c mutau_TES0p996 -s DY --das
-#pico.py $job -y UL2018 -c mutau_TES0p998 -s DY --das
-#pico.py $job -y UL2018 -c mutau_TES1p000 -s DY --das
-#pico.py $job -y UL2018 -c mutau_TES1p002 -s DY --das
-#pico.py $job -y UL2018 -c mutau_TES1p004 -s DY --das
-#pico.py $job -y UL2018 -c mutau_TES1p006 -s DY --das
-#pico.py $job -y UL2018 -c mutau_TES1p008 -s DY --das
-#pico.py $job -y UL2018 -c mutau_TES1p010 -s DY --das
-#pico.py $job -y UL2018 -c mutau_TES1p012 -s DY --das
-#pico.py $job -y UL2018 -c mutau_TES1p014 -s DY --das
-#pico.py $job -y UL2018 -c mutau_TES1p016 -s DY --das
-#pico.py $job -y UL2018 -c mutau_TES1p018 -s DY --das
-#pico.py $job -y UL2018 -c mutau_TES1p020 -s DY --das
-#pico.py $job -y UL2018 -c mutau_TES1p022 -s DY --das
-#pico.py $job -y UL2018 -c mutau_TES1p024 -s DY --das
-#pico.py $job -y UL2018 -c mutau_TES1p026 -s DY --das
-#pico.py $job -y UL2018 -c mutau_TES1p028 -s DY --das
-#pico.py $job -y UL2018 -c mutau_TES1p030 -s DY --das
+pico.py $job -y UL2018 -c mutau_TES0p970 -s DY --das
+pico.py $job -y UL2018 -c mutau_TES0p972 -s DY --das
+pico.py $job -y UL2018 -c mutau_TES0p974 -s DY --das
+pico.py $job -y UL2018 -c mutau_TES0p976 -s DY --das
+pico.py $job -y UL2018 -c mutau_TES0p978 -s DY --das
+pico.py $job -y UL2018 -c mutau_TES0p980 -s DY --das
+pico.py $job -y UL2018 -c mutau_TES0p982 -s DY --das
+pico.py $job -y UL2018 -c mutau_TES0p984 -s DY --das
+pico.py $job -y UL2018 -c mutau_TES0p986 -s DY --das
+pico.py $job -y UL2018 -c mutau_TES0p988 -s DY --das
+pico.py $job -y UL2018 -c mutau_TES0p990 -s DY --das
+pico.py $job -y UL2018 -c mutau_TES0p992 -s DY --das
+pico.py $job -y UL2018 -c mutau_TES0p994 -s DY --das
+pico.py $job -y UL2018 -c mutau_TES0p996 -s DY --das
+pico.py $job -y UL2018 -c mutau_TES0p998 -s DY --das
+pico.py $job -y UL2018 -c mutau_TES1p000 -s DY --das
+pico.py $job -y UL2018 -c mutau_TES1p002 -s DY --das
+pico.py $job -y UL2018 -c mutau_TES1p004 -s DY --das
+pico.py $job -y UL2018 -c mutau_TES1p006 -s DY --das
+pico.py $job -y UL2018 -c mutau_TES1p008 -s DY --das
+pico.py $job -y UL2018 -c mutau_TES1p010 -s DY --das
+pico.py $job -y UL2018 -c mutau_TES1p012 -s DY --das
+pico.py $job -y UL2018 -c mutau_TES1p014 -s DY --das
+pico.py $job -y UL2018 -c mutau_TES1p016 -s DY --das
+pico.py $job -y UL2018 -c mutau_TES1p018 -s DY --das
+pico.py $job -y UL2018 -c mutau_TES1p020 -s DY --das
+pico.py $job -y UL2018 -c mutau_TES1p022 -s DY --das
+pico.py $job -y UL2018 -c mutau_TES1p024 -s DY --das
+pico.py $job -y UL2018 -c mutau_TES1p026 -s DY --das
+pico.py $job -y UL2018 -c mutau_TES1p028 -s DY --das
+pico.py $job -y UL2018 -c mutau_TES1p030 -s DY --das
 
-pico.py $job -y UL2018 -c mutau_LTFUp -s DY TT W*J --das
-pico.py $job -y UL2018 -c mutau_LTFDown -s DY TT W*J --das
+#pico.py $job -y UL2018 -c mutau_LTFUp -s DY TT W*J --das
+#pico.py $job -y UL2018 -c mutau_LTFDown -s DY TT W*J --das
 
-pico.py $job -y UL2018 -c mutau_JTFUp -s DY TT W*J --das
-pico.py $job -y UL2018 -c mutau_JTFDown -s DY TT W*J --das
+#pico.py $job -y UL2018 -c mutau_JTFUp -s DY TT W*J --das
+#pico.py $job -y UL2018 -c mutau_JTFDown -s DY TT W*J --das
