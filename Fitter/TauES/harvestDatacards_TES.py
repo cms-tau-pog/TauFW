3fe8ff5427c07d9d544718f7f809793f1c1ce038
Saskia Falke <saskia.falke@cern.ch>
2022-02-24 15:32:08 +0100

    Fully functional

--- Fitter/TauES/harvestDatacards_TES.py
+++ Fitter/TauES/harvestDatacards_TES.py
@@ -3,7 +3,6 @@
 
 import ROOT; ROOT.PyConfig.IgnoreCommandLineOptions = True
 import os, sys, re
-from argparse import ArgumentParser
 import yaml
 import CombineHarvester.CombineTools.ch as ch
 from CombineHarvester.CombineTools.ch import CombineHarvester, MassesFromRange, SystMap, BinByBinFactory, CardWriter, SetStandardBinNames, AutoRebin
@@ -12,34 +11,11 @@ from CombineHarvester.CombinePdfs.morphing import BuildRooMorphing
 import ROOT
 from ROOT import RooWorkspace, TFile, RooRealVar
 
-argv = sys.argv
-description = '''This script makes datacards with CombineHarvester.'''
-parser = ArgumentParser(prog="harvesterDatacards_TES",description=description,epilog="Succes!")
-parser.add_argument('-y', '--year', dest='year', choices=['2016','2017','2018','UL2016_preVFP','UL2016_postVFP','UL2017','UL2018'], type=str, default=2018, action='store', help="select year")
-parser.add_argument('-c', '--config', dest='config', type=str, default='TauES/config/defaultFitSetupTES_mutau.yml', action='store', help="set config file containing sample & fit setup")
-parser.add_argument('-t', '--tag', dest='tags', type=str, nargs='+', default=[ ], action='store', metavar='TAG', help="tag for a file names")
-parser.add_argument('-e', '--extra-tag', dest='extratag', type=str, default="", action='store', metavar='TAG', help="extra tag for output files")
-parser.add_argument('-o', '--obs', dest='observables', type=str, nargs='*', default=[], action='store', metavar='VARIABLE', help="name of observable for TES measurement")
-parser.add_argument('-n', '--no-shapes', dest='noShapes', default=False, action='store_true', help="do not include shape uncertainties")
-parser.add_argument('-M', '--multiDimFit', dest='multiDimFit', default=False, action='store_true', help="assume multidimensional fit with a POI for each DM")
-parser.add_argument('-v', '--verbose', dest='verbose', default=False, action='store_true', help="set verbose")
-args = parser.parse_args()
 
-## Open and import information from config file here to be publicly accessible in all functions
-print "Using configuration file: %s"%args.config
-with open(args.config, 'r') as file:
-  setup = yaml.safe_load(file)
-
-channel         = setup["channel"].replace("mu","m").replace("tau","t")
-verbosity       = 1 if args.verbose else 0
-doShapes        = not args.noShapes #and False
-multiDimFit     = args.multiDimFit
-observables     = []
-for obs in setup["observables"]:
-  observables.append(obs)
-
-def harvest(channel,var,year,**kwargs):
+def harvest(setup, year, obs, **kwargs):
     """Harvest cards."""
+
+    channel         = setup["channel"].replace("mu","m").replace("tau","t")
     
     tag         = kwargs.get('tag',        ""               )
     extratag    = kwargs.get('extratag',   ""               )
@@ -47,6 +23,8 @@ def harvest(channel,var,year,**kwargs):
     analysis    = kwargs.get('analysis',   'ztt'            )
     indir       = kwargs.get('indir',      'input_%s'%year  )
     outdir      = kwargs.get('outdir',     'output_%s'%year )
+    multiDimFit = kwargs.get('multiDimFit')
+    verbosity   = kwargs.get('verbosity')
     outtag      = tag+extratag
     TIDWP       = 'Medium' if 'Medium' in outtag else 'Tight'
     
@@ -62,26 +40,24 @@ def harvest(channel,var,year,**kwargs):
 
     cats = []
     icat = 0
-    # each variable can have a subset of regions in which it is fitted
-    # defined in config file under this variable entry
-    if "fitRegions" in setup["observables"][var]:
-      for region in setup["observables"][var]["fitRegions"]:
+    # each variable can have a subset of regions in which it is fitted defined in config file under this variable entry
+    if "fitRegions" in setup["observables"][obs]:
+      for region in setup["observables"][obs]["fitRegions"]:
         icat += 1
         cats.append((icat, region))
-    # if not given, assume all defined regions should be fitted 
-    # (be careful with potential overlap!)
+    # if not given, assume all defined regions should be fitted (be careful with potential overlap!)
     elif "regions" in setup:
       for region in setup["regions"]:
         icat += 1
         cats.append((icat,region))
-    print "Fit regions for observable %s: %s"%(var, cats)
+    print "Fit regions for observable %s: %s"%(obs, cats)
 
     tesshifts = [ "%.3f"%tes for tes in setup["TESvariations"]["values"] ]
 
     harvester = CombineHarvester()
     harvester.AddObservations(['*'], [analysis], [era], [channel], cats)
     harvester.AddProcesses(['*'], [analysis], [era], [channel], backgrounds, cats, False)
-    # CAVEAT: This is specific to mutau channel; should see how to generalise!
+    # CAVEAT: Assume we always want to fit TES as POI; if running for mumu channel, everything will be bkg
     harvester.AddProcesses(tesshifts, [analysis], [era], [channel], signals, cats, True)
     
     # FILTER ## CAVEAT!!! potentially needed to not add zero backgrounds
@@ -103,7 +79,7 @@ def harvest(channel,var,year,**kwargs):
    
     # EXTRACT SHAPES
     print green(">>> extracting shapes...")
-    filename = "%s/%s_%s_tes_%s.inputs-%s%s.root"%(indir,analysis,channel,var,era,tag)
+    filename = "%s/%s_%s_tes_%s.inputs-%s%s.root"%(indir,analysis,channel,obs,era,tag)
     print ">>>   file %s"%(filename)
     ## For now assume that everything that is varied by TES is signal, and everything else is background
     ## Could be revised if wanting to leave the possibility to do other variations or fit normalisation (e.g. for combined TES & ID SF fit)
@@ -125,7 +101,7 @@ def harvest(channel,var,year,**kwargs):
     # BINS
     print green(">>> generating unique bin names...")
     bins = harvester.bin_set() # categories
-    #SetStandardBinNames(harvester,"%s_$BINID_$ERA"%(var))
+    #SetStandardBinNames(harvester,"%s_$BINID_$ERA"%(obs))
     
     # Bin-by-bin uncertainties
     if "fitSpecs" in setup and "doBBB" in setup["fitSpecs"] and setup["fitSpecs"]["doBBB"] != "":
@@ -154,7 +130,7 @@ def harvest(channel,var,year,**kwargs):
     # MORPHING
     print green(">>> morphing...")
     debugdir  = ensureDirectory("debug")
-    debugfile = TFile("%s/morph_debug_%s_%s_%s%s.root"%(debugdir,year,channel,var,outtag), 'RECREATE')
+    debugfile = TFile("%s/morph_debug_%s_%s_%s%s.root"%(debugdir,year,channel,obs,outtag), 'RECREATE')
     verboseMorph = verbosity>0
     for bin, poi in zip(bins,pois):
       print '>>>   bin "%s"...'%(bin)
@@ -194,8 +170,8 @@ def harvest(channel,var,year,**kwargs):
     
     # WRITER
     print green(">>> writing datacards...")
-    datacardtxt  = "$TAG/$ANALYSIS_$CHANNEL_%s-$BINID%s-$ERA.txt"%(var,outtag)
-    datacardroot = "$TAG/$ANALYSIS_$CHANNEL_%s%s.input-$ERA.root"%(var,outtag)
+    datacardtxt  = "$TAG/$ANALYSIS_$CHANNEL_%s-$BINID%s-$ERA.txt"%(obs,outtag)
+    datacardroot = "$TAG/$ANALYSIS_$CHANNEL_%s%s.input-$ERA.root"%(obs,outtag)
     writer = CardWriter(datacardtxt,datacardroot)
     writer.SetVerbosity(verbosity)
     writer.SetWildcardMasses([ ])
@@ -238,32 +214,46 @@ def ensureDirectory(dirname):
 
 
 
-def main():
+def main(args):
+
+    ## Open and import information from config file here to be publicly accessible in all functions
+    print "Using configuration file: %s"%args.config
+    with open(args.config, 'r') as file:
+        setup = yaml.safe_load(file)
+
+    verbosity = 1 if args.verbose else 0
+    observables = []
+    for obs in setup["observables"]:
+        observables.append(obs)
     
-    year     = args.year
-    vars = observables
-    indir    = "./input"
-    extratag = args.extratag
-    if multiDimFit:
-      extratag += "_MDF"
-    
-    ## To do: check!
-    print "We need a tag!!!!!!!!!!!!!"
+    indir = "./input"
+    if args.multiDimFit:
+        args.extratag += "_MDF"
+
     for tag in args.tags:
-      vars2 = vars[:]
-      if "_0p"  in tag: vars2 = [ v for v in vars2 if v!='m_vis' ]
-      if "_45"  in tag: vars2 = [ v for v in vars2 if v!='m_2'   ]
-      if "_100" in tag: vars2 = [ v for v in vars2 if v!='m_2'   ]
-      if "_200" in tag: vars2 = [ v for v in vars2 if v!='m_2'   ]
-      
-      print "producing datacards for %s"%(year)
-      for var in vars2:
-        print "producing datacards for %s"%(var)
-        harvest(channel,var,year,tag=tag,extratag=extratag,indir=indir)
+        print "producing datacards for %s"%(args.year)
+        for obs in observables:
+            print "producing datacards for %s"%(obs)
+            harvest(setup,args.year,obs,tag=tag,extratag=args.extratag,indir=indir,multiDimFit=args.multiDimFit,verbosity=verbosity)
     
 
+
+
 if __name__ == '__main__':
-    main()
-    print ">>>\n>>> done harvesting\n"
+
+  from argparse import ArgumentParser
+  argv = sys.argv
+  description = '''This script makes datacards with CombineHarvester.'''
+  parser = ArgumentParser(prog="harvesterDatacards_TES",description=description,epilog="Succes!")
+  parser.add_argument('-y', '--year', dest='year', choices=['2016','2017','2018','UL2016_preVFP','UL2016_postVFP','UL2017','UL2018'], type=str, default=2018, action='store', help="select year")
+  parser.add_argument('-c', '--config', dest='config', type=str, default='TauES/config/defaultFitSetupTES_mutau.yml', action='store', help="set config file containing sample & fit setup")
+  parser.add_argument('-t', '--tag', dest='tags', type=str, nargs='+', default=[ ], action='store', metavar='TAG', help="tag for a file names")
+  parser.add_argument('-e', '--extra-tag', dest='extratag', type=str, default="", action='store', metavar='TAG', help="extra tag for output files")
+  parser.add_argument('-M', '--multiDimFit', dest='multiDimFit', default=False, action='store_true', help="assume multidimensional fit with a POI for each DM")
+  parser.add_argument('-v', '--verbose', dest='verbose', default=False, action='store_true', help="set verbose")
+  args = parser.parse_args()
+
+  main(args)
+  print ">>>\n>>> done harvesting\n"
     
 
