da8dc2d32c2f9de8058712629d09fdfcea644da0
IzaakWN <iwn_@hotmail.com>
2022-02-25 13:53:32 +0100

    update instructions

--- Plotter/python/macros/Unroll.cxx
+++ Plotter/python/macros/Unroll.cxx
@@ -27,14 +27,14 @@
 //     hist1d = TH2D('hist1d',"Unrolled",nxbins*nybins,1,21) # unrolled with bin numbers 1 to nxbins*nybins
 //     Unroll.SetBins(hist2d) # set bin axis globally for unrolling in Unroll::GetBin
 //     tree.Draw("Unroll::GetBin(x,y) >> hist1d") # create unrolled 1D histogram (with under/overflow)
-//     hist2d_rollup = Unroll.RollUp(hist1d,"Rolled up",hist2d) # roll 1D back up to 2D
 //   Unroll existing 2D histogram:
 //     gROOT.ProcessLine(".L Unroll.cxx+O")
 //     from ROOT import Unroll
 //     hist2d = TH2D('hist2d',"y vs. x",nxbins,0,100,nybins,0,100) # original 2D
-//     hist1d = TH2D('hist1d',"Unrolled",nxbins*nybins,1,21) # unrolled with bin numbers 1 to nxbins*nybins
 //     tree.Draw("y:x >> hist2d") # create 2D histogram
-//     hist1d_unroll = Unroll.Unroll(hist2d,"Unrolled") # unroll 2D to 1D (without under/overflow)
+//     hist1d = Unroll.Unroll(hist2d,"Unrolled") # unroll 2D to 1D (without under/overflow)
+//   Roll back up 1D to 2D:
+//     hist2d_rollup = Unroll.RollUp(hist1d,"Rolled up",hist2d) # roll 1D back up to 2D
 //   
 //   4D migration / response matrix unrolled to 2D:
 //     hist4d = TH2D('h_response',"Response",20,1,21,20,1,21) # original 2D
