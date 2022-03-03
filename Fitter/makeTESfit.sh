963e61255c67daf514dc7486d6d35a2cfb4be532
Saskia Falke <saskia.falke@cern.ch>
2022-03-03 09:53:23 +0100

    fit script

--- Fitter/makeTESfit.sh
+++ Fitter/makeTESfit.sh
@@ -1,4 +1,3 @@
-CONFIG="TauES/config/defaultFitSetupTES_mutau.yml"
 DMS="DM0 DM1 DM10 DM11"
 VARS="m_vis m_2"
 YEAR="UL2018"
@@ -12,7 +11,7 @@ POI_OPTS="-P tes --setParameterRanges tes=${RANGE} -m 90 --setParameters r=1 --f
 XRTD_OPTS="--X-rtd FITTER_NEW_CROSSING_ALGO --X-rtd FITTER_NEVER_GIVE_UP --X-rtd FITTER_BOUND" #--X-rtd FITTER_DYN_STEP
 CMIN_OPTS="--cminFallbackAlgo Minuit2,Migrad,0:0.5 --cminFallbackAlgo Minuit2,Migrad,0:1.0 --cminPreScan" # --cminPreFit 1 --cminOldRobustMinimize 
 
-./TauES/harvestDatacards_TES.py -y $YEAR -t $TAG -e $EXTRATAG -c $CONFIG
+./TauES/harvestDatacards_TES.py -y $YEAR -t $TAG -e $EXTRATAG -c $1
 
 for DM in $DMS; do
     echo $DM
@@ -33,8 +32,7 @@ mv higgsCombine*root output_$YEAR
 
 for var in $VARS; do
 
-    ./TauES/plotParabola_TES.py -y $YEAR -t $TAG -d $DMS -e $EXTRATAG -a -o $var -r $RANGE
-    ./TauES/plotParabola_TES.py -y $YEAR -t $TAG -d $DMS -e $EXTRATAG -r $RANGE -s
-    ./TauES/plotPostFitScan_TES.py -y $YEAR -t $TAG -d $DMS -e $EXTRATAG -o $var -r $RANGE
+    ./TauES/plotParabola_TES.py -y $YEAR -t $TAG -e $EXTRATAG -r $RANGE -s -a -c $1
+    ./TauES/plotPostFitScan_TES.py -y $YEAR -t $TAG -e $EXTRATAG -o $var -r $RANGE
 
 done
