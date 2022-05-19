397d5720b6eec9ad83ca88f12629eb8ac5e7afd9
IzaakWN <iwn_@hotmail.com>
2022-05-19 14:40:15 +0200

    add correlation, bias & channel compatibility test

---
+++ Fitter/python/plot/correlation.py
@@ -0,0 +1,360 @@
+#! /usr/bin/env python
+# Author: Izaak Neutelings (April 2021)
+# https://github.com/cms-analysis/HiggsAnalysis-CombinedLimit/blob/102x/test/diffNuisances.py
+import re
+from array import array
+from ROOT import gROOT, gPad, gStyle, Double, TFile, TCanvas, TLegend, TLatex, TH1F, TLine, TColor,\
+                 kBlack, kGray, kBlue, kRed, kGreen, kYellow, kOrange, kMagenta, kTeal, kAzure, kBlackBody
+import TauFW.Plotter.plot.CMSStyle as CMSStyle
+from math import log10, ceil, floor
+gROOT.SetBatch(True)
+gStyle.SetOptTitle(0)
+
+# CUSTOM COLOR PALETTE, see core/base/src/TColor.cxx
+red   = array('d',[ 0.80, 0.90, 1.00, 0.60, 0.02, ])
+green = array('d',[ 0.20, 0.80, 1.00, 0.80, 0.20, ])
+blue  = array('d',[ 0.10, 0.60, 1.00, 0.90, 0.65, ])
+stops = array('d',[i/(len(red)-1.) for i in xrange(0,len(red))])
+FI    =  TColor.CreateGradientColorTable(len(red), stops, red, green, blue, 100)
+kMyTemperature = array('i',[ FI+i for i in xrange(100)])
+gStyle.SetPalette(100,kMyTemperature)
+
+
+def plotCorrelationHist(fname,**kwargs):
+  """Draw correlation of nuisance parameters from FitDiagnostics output."""
+  print green("\n>>> plotCorrelationHist")
+  pois       = kwargs.get('poi',    "r"        )
+  filters    = kwargs.get('filter', [ ]        )
+  nbins      = kwargs.get('nbins',  40         )
+  xmin       = kwargs.get('xmin',   -1         )
+  xmax       = kwargs.get('xmax',   1          )
+  title      = kwargs.get('title',  ""         )
+  name       = kwargs.get('name',   ""         )
+  indir      = kwargs.get('indir',  "output"   )
+  outdir     = kwargs.get('outdir', "plots"    )
+  xtitle     = kwargs.get('xtitle', "Correlation to signal strength r"  )
+  ytitle     = kwargs.get('ytitle', "Nuisance parameters" )
+  tag        = kwargs.get('tag',    ""         )
+  ptag       = kwargs.get('ptag',   ""         )
+  logy       = kwargs.get('logy',   True       )
+  ensuredir(outdir)
+  pname = "%s/correlation-%s%s%s"%(outdir,name,tag,ptag)
+  colors = [ kBlue, kRed, kGreen, kMagenta, kOrange, kTeal, kAzure+2, kYellow-3 ]
+  if not isinstance(pois,list):
+    pois = [pois]
+  if not isinstance(filters,list):
+    filters = [filters]
+  for i, filter in enumerate(filters):
+    filters[i] = re.compile(filter)
+  
+  file  = ensureTFile(fname)
+  fits  = [
+    #('fit_b',"B-only"), # r fixed in B-only...
+    ('fit_s',"S+B"),
+  ]
+  hists = [ ]
+  cuts  = { c: 0 for c in [0.10,0.25,0.5,0.75] }
+  pcut  = 0.15 # for printing
+  for fitname, fittitle in fits:
+    print ">>> File %s:%s"%(fname,fitname)
+    fit   = file.Get(fitname) # fit results
+    if not fit:
+      warning(">>>   Could not get %r!"%(fit))
+    hist  = TH1F("corr_"+fitname,fittitle,nbins,-1.,1.)
+    #hist.SetDirectory(0)
+    hists.append(hist)
+    fpars = fit.floatParsFinal()
+    npars = fpars.getSize()
+    rpars = [ ] # list of POIs for correlation computation
+    for poi in pois:
+      npars_ = 0
+      print ">>> Looking for %r..."%(poi)
+      if any(c in poi for c in '.*+()[]'): # assume regexp
+        exp_poi = re.compile(poi)
+        for i in range(npars): # find all matches to POI regexp
+          rpar = fpars.at(i)
+          if rpar in rpars: continue # no double counting
+          if not exp_poi.search(rpar.GetName()): continue # no match
+          rpars.append(rpar)
+          npars_ += 1
+        print ">>> Found %d parameters for %r"%(npars_,poi)
+      else: # find by exact name
+        rpar = fpars.find(poi)
+        rpars.append(rpar)
+    print ">>> Number of POIs for correlation: %d"%(len(rpars))
+    #corrs = fit.correlation(rpar)
+    #corrs = fit.correlation(poi)
+    for i in range(npars):
+    #for i in range(corrs.getSize()):
+      # https://root.cern/doc/v610/classRooFitResult.html#a4b3c891a43f776fbe33bafbaaa2708de
+      npar = fpars.at(i)
+      if filters and not any(f.search(npar.GetName()) for f in filters): continue
+      ridx = rpars.index(npar)+1 if npar in rpars else 0 # avoid double counting multiple POIs (assume same order)
+      #print len(rpars[ridx:]), ridx, npar.GetName()
+      for rpar in rpars[ridx:]: # get correlation to each POI
+        #if npar==rpar: continue # redundant with ridx
+        corr = fit.correlation(rpar,npar)
+        for cut in cuts: # cut 'n count
+          if abs(corr)>cut:
+            cuts[cut] += 1
+        if abs(corr)>pcut: # print
+          print ">>>   %+6.3f > %.2f:  %s to %s"%(corr,pcut,npar.GetName(),rpar.GetName())
+        #corr = corrs.at(i)
+        #print i, corr, rpar, npar
+        hist.Fill(corr)
+  
+  ymargin = 1.15
+  if logy:
+    ymin = 0.6
+    hmax = max(ymargin*h.GetMaximum() for h in hists)
+    span = ymargin*abs(log10(hmax/ymin))
+    ymax = ymin*(10**span)
+  else:
+    ymin = 0
+    ymax = max(1.15*h.GetMaximum() for h in hists)
+  canvas = TCanvas("canvas","canvas",100,100,800,650)
+  canvas.SetFillColor(0)
+  canvas.SetBorderMode(0)
+  canvas.SetFrameFillStyle(0)
+  canvas.SetFrameBorderMode(0)
+  canvas.SetMargin(0.11,0.05,0.13,0.07) # LRBT
+  canvas.SetTickx(0)
+  canvas.SetTicky(0)
+  canvas.SetGrid()
+  if logy:
+    canvas.SetLogy()
+  
+  #textsize   = 0.045 if npars==1 else 0.036
+  #lineheight = 0.055 if npars==1 else 0.045
+  #x1, width  = 0.42, 0.25
+  #y1, height = 0.15, lineheight*(ceil(npars/3.) if npars>6 else ceil(npars/2.) if npars>3 else npars)
+  #if title:
+  #  height += lineheight
+  #if npars>6:
+  #  if isBBB: width = 0.62; x1 = 0.25
+  #  else:     width = 0.70; x1 = 0.20
+  #elif npars>3: 
+  #  width = 0.52; x1 = 0.36
+  #legend = TLegend(x1,y1,x1+width,y1+height)
+  #legend.SetTextSize(textsize)
+  #legend.SetBorderSize(0)
+  #legend.SetFillStyle(0)
+  #legend.SetFillColor(0)
+  #if title:
+  #  legend.SetTextFont(62)
+  #  legend.SetHeader(title) 
+  #legend.SetTextFont(42)
+  
+  frame = canvas.DrawFrame(xmin,ymin,xmax,ymax)
+  frame.GetYaxis().SetTitleSize(0.060)
+  frame.GetXaxis().SetTitleSize(0.060)
+  frame.GetXaxis().SetLabelSize(0.050)
+  frame.GetYaxis().SetLabelSize(0.050)
+  frame.GetXaxis().SetLabelOffset(0.01)
+  frame.GetYaxis().SetLabelOffset(0.001 if logy else 0.01)
+  frame.GetXaxis().SetTitleOffset(0.96)
+  frame.GetYaxis().SetTitleOffset(0.90 if logy else 0.86)
+  frame.GetXaxis().SetNdivisions(508)
+  frame.GetYaxis().SetTitle(ytitle)
+  frame.GetXaxis().SetTitle(xtitle)
+  
+  for i, hist in enumerate(hists):
+    hist.SetLineColor(colors[i%len(colors)])
+    hist.SetLineWidth(2)
+    hist.SetMarkerColor(colors[i%len(colors)])
+    hist.SetMarkerSize(1)
+    hist.SetLineStyle(1)
+    hist.Draw('HIST SAME')
+  
+  #for param in paramsleg:
+  #  legend.AddEntry(graphs[param],formatParameter(param),'lep')
+  #legend.Draw()
+  
+  # CUTS
+  latex = TLatex()
+  latex.SetTextSize(0.045)
+  latex.SetNDC() # normalized coordinates
+  ntot = hist.GetEntries()
+  x1 = 0.14
+  x2 = x1 + 0.23+0.02*floor(log10(ntot))
+  y1 = 0.90
+  if title:
+    latex.SetTextAlign(13)
+    latex.SetTextFont(62)
+    latex.DrawLatex(x1,y1,title)
+    y1 -= 0.055
+  latex.SetTextFont(42)
+  for i, cut in enumerate(sorted(cuts.keys())):
+    npass = cuts[cut]
+    latex.SetTextAlign(13)
+    #latex.DrawLatex(0.14,0.89-0.05*i,"|#rho| > %.2f:"%(cut,npass))
+    latex.DrawLatex(x1,y1,"|#rho| > %.2f:"%(cut))
+    latex.SetTextAlign(33)
+    latex.DrawLatex(x2,y1,"%d/%d"%(npass,ntot))
+    y1 -= 0.052
+  
+  CMS_lumi.relPosX = 0.12
+  CMS_lumi.CMS_lumi(canvas,13,0)
+  gPad.SetTicks(1,1)
+  gPad.Modified()
+  frame.Draw('SAMEAXIS')
+  canvas.RedrawAxis()
+  canvas.SaveAs(pname+".png")
+  canvas.SaveAs(pname+".pdf")
+  canvas.Close()
+  file.Close()
+
+
+def drawCorrelationMatrix(hist,pname,**kwargs):
+  """Help function to plot correlation matrix."""
+  print ">>> drawCorrelationMatrix(%r)"%(pname)
+  zmin   = kwargs.get('zmin',   -100.0     )
+  zmax   = kwargs.get('zmax',   100.0      )
+  title  = kwargs.get('title',  ""         )
+  name   = kwargs.get('name',   ""         )
+  indir  = kwargs.get('indir',  "output"   )
+  outdir = kwargs.get('outdir', "plots"    )
+  ztitle = kwargs.get('ztitle', "Correlation [%]"  ) # #rho
+  tag    = kwargs.get('tag',    ""         )
+  ptag   = kwargs.get('ptag',   ""         )
+  logz   = kwargs.get('logz',   True       )
+  msize  = kwargs.get('msize',  1.0        )
+  xtitle = kwargs.get('xtitle', "Bin"      )
+  ytitle = kwargs.get('ytitle', xtitle     )
+  ndiv   = kwargs.get('ndiv',   0          ) # 508
+  xcats  = kwargs.get('xcats',  [ ]        )
+  ycats  = kwargs.get('ycats',  [ ]        )
+  nxbins = hist.GetXaxis().GetNbins()
+  nybins = hist.GetYaxis().GetNbins()
+  
+  # SCALE
+  canvasH  = 180+42*max(22,nybins)
+  canvasW  = 310+55*max(22,nxbins)
+  scaleH   =  800./canvasH
+  scaleW   = 1000./canvasW
+  scaleF   = 600./(canvasH-160)
+  tmargin  = 0.053
+  bmargin  = 0.14
+  lmargin  = 0.14
+  rmargin  = 0.16
+  xlsize   = min(0.050*scaleF,0.03) # x label size
+  ylsize   = min(0.056*scaleF,0.03) # x label size
+  dsize    = min(0.15*scaleF,0.03) # divider size
+  print ">>>   canvas %d x %d"%(canvasW,canvasH)
+  print ">>>   scaleF=%.5g, scaleH=%.5g, scaleW=%.5g"%(scaleF,scaleH,scaleW)
+  
+  canvas = TCanvas('canvas','canvas',100,100,canvasW,canvasH)
+  canvas.SetFillColor(0)
+  canvas.SetBorderMode(0)
+  canvas.SetFrameFillStyle(0)
+  canvas.SetFrameBorderMode(0)
+  canvas.SetMargin(lmargin,rmargin,bmargin,tmargin) # LRBT
+  canvas.SetTicks(1,1)
+  canvas.SetGrid(0,0)
+  canvas.cd()
+  
+  frame = hist
+  frame.GetXaxis().SetLabelSize(0.050*scaleF)
+  frame.GetYaxis().SetLabelSize(0.056*scaleF)
+  frame.GetZaxis().SetLabelSize(0.055*scaleF)
+  frame.GetZaxis().SetLabelSize(0.030)
+  frame.GetXaxis().SetLabelOffset(0.005*scaleF)
+  frame.GetYaxis().SetLabelOffset(0.003*scaleF)
+  frame.GetXaxis().SetTickLength(0)
+  frame.GetYaxis().SetTickLength(0)
+  frame.GetXaxis().SetNdivisions(ndiv,False)
+  frame.GetYaxis().SetNdivisions(ndiv,False)
+  frame.GetXaxis().SetTitleSize(0.042)
+  frame.GetYaxis().SetTitleSize(0.042)
+  frame.GetZaxis().SetTitleSize(0.042)
+  frame.GetXaxis().SetTitleOffset(1.4)
+  frame.GetYaxis().SetTitleOffset(1.5)
+  frame.GetZaxis().SetTitleOffset(1.1) #97*rmargin/0.16
+  frame.GetXaxis().SetTitle(xtitle)
+  frame.GetYaxis().SetTitle(ytitle)
+  frame.GetZaxis().SetTitle(ztitle)
+  frame.GetZaxis().CenterTitle(True)
+  #gStyle.SetPalette(kBlackBody)
+  #frame.SetContour(3);
+  #gStyle.SetPaintTextFormat(".3g")
+  gStyle.SetPaintTextFormat(".0f")
+  frame.SetMarkerSize(1.4*scaleF*msize)
+  #frame.SetMarkerColor(kRed)
+  frame.SetMinimum(zmin)
+  frame.SetMaximum(zmax)
+  hist.Draw('COLZ TEXT')
+  
+  # DIVIDERS
+  lines = [ ]
+  texts = [ ]
+  xbody = 1.-lmargin-rmargin
+  ybody = 1.-tmargin-bmargin
+  dx    = (4.9*xlsize+0.6*dsize)*nxbins/xbody
+  dy    = (4.2*ylsize+0.7*dsize)*nybins/ybody
+  #print ">>> dsize=%s, xlsize=%s, nxbins=%s, xbody=%s => dx=%s"%(dsize,xlsize,nxbins,xbody,dx)
+  #print ">>> dsize=%s, ylsize=%s, nybins=%s, ybody=%s => dy=%s"%(dsize,ylsize,nybins,ybody,dy)
+  if xcats:
+    latex = TLatex()
+    latex.SetTextSize(dsize)
+    latex.SetTextFont(42)
+    latex.SetTextColor(kRed+1)
+    latex.SetTextAlign(21)
+    latex.SetTextAngle(0)
+    texts.append(latex)
+    for i, (x, cat) in enumerate(xcats):
+      xnext = xcats[i+1][0] if i+1<len(xcats) else nxbins
+      #ymin, ymax = -0.8./scaleF, nybins
+      ymin, ymax = -dx, nybins
+      latex.DrawLatex((x+xnext)/2,ymin,cat)
+      if x>0:
+        #TLine.DrawLineNDC(x,0,x,nbins)
+        line = TLine(x,ymin,x,ymax)
+        line.SetLineWidth(3)
+        line.SetLineColor(kRed+1)
+        line.Draw('SAME')
+        lines.append(line) # store in memory
+  if ycats:
+    latex = TLatex()
+    latex.SetTextSize(dsize)
+    latex.SetTextFont(42)
+    latex.SetTextColor(kRed+1)
+    latex.SetTextAlign(23)
+    latex.SetTextAngle(90)
+    texts.append(latex)
+    for i, (y, cat) in enumerate(ycats):
+      ynext = ycats[i+1][0] if i+1<len(ycats) else nybins
+      xmin, xmax = -dy, nxbins
+      latex.DrawLatex(xmin,(y+ynext)/2,cat)
+      if x>0:
+        line = TLine(xmin,y,xmax,y)
+        line.SetLineWidth(5)
+        line.SetLineColor(kRed+1)
+        line.Draw('SAME')
+        lines.append(line) # store in memory
+  
+  # TEXT
+  if title:
+    latex = TLatex()
+    latex.SetTextSize(0.038)
+    latex.SetTextAlign(11)
+    latex.SetTextFont(42)
+    latex.SetTextAngle(0)
+    y = 0.1*bmargin
+    for line in title.split('\n'):
+      latex.DrawLatexNDC(0.1*lmargin,y,line)
+      y -= 0.05
+    texts.append(latex)
+  
+  CMS_lumi.relPosX = 0.14
+  CMS_lumi.CMS_lumi(canvas,13,0)
+  canvas.SetTicks(1,1)
+  canvas.SetGrid(0,0)
+  canvas.Modified()
+  frame.Draw('SAMEAXIS')
+  
+  canvas.SaveAs(pname+".png")
+  for line in lines:
+    line.SetLineWidth(1)
+  canvas.SaveAs(pname+".pdf")
+  canvas.Close()
+  
---
+++ Fitter/scripts/plotBiasTest.py
@@ -0,0 +1,384 @@
+#! /usr/bin/env python
+# Author: Izaak Neutelings (January 2019)
+import os, sys, re
+import json
+import array
+from math import ceil
+from argparse import ArgumentParser
+import ROOT; ROOT.PyConfig.IgnoreCommandLineOptions = True
+from ROOT import gROOT, gStyle, gPad, TFile, TChain, TCanvas, TH1F, TGraph, TGraphErrors, TLegend, TLatex, TLine,\
+                 kBlack, kBlue, kRed, kGreen, kGray, kOrange
+gROOT.SetBatch(True)
+gStyle.SetOptTitle(False)
+gStyle.SetEndErrorSize(4)
+gStyle.SetErrorX(0)
+
+argv = sys.argv
+description = '''Plots bias tests and comparisons.'''
+parser = ArgumentParser(prog="plotBias",description=description,epilog="Good luck!")
+parser.add_argument( "filenames",          type=str, nargs='+', action='store',
+                                           help="file from fitDiagnostics" ),
+parser.add_argument( '-r', "--rinj",       dest="rinj", type=float, default=0, action='store',
+                                           help="injected signal strength" )
+parser.add_argument( '-t', "--text",       dest="text", type=str, default="", action='store',
+                                           help="extra text" )
+parser.add_argument( '-o', "--outfile",    dest="outfile", type=str, default="biastest", action='store',
+                     metavar="FILENAME",   help="name of output figure" )
+parser.add_argument( '-v', "--verbose",    dest="verbose",  default=False, action='store_true',
+                                           help="set verbose" )
+args = parser.parse_args()
+
+
+def warning(string,**kwargs):
+  return ">>> \033[1m\033[93m%sWarning!\033[0m\033[93m %s\033[0m"%(kwargs.get('pre',""),string)
+  
+
+def columnize(oldlist,ncol=2):
+  """Transpose lists into n columns, useful for TLegend,
+  e.g. [1,2,3,4,5,6,7] -> [1,5,2,6,3,7,4] for ncol=2."""
+  if ncol<2:
+    return oldlist
+  parts   = partition(oldlist,ncol)
+  collist = [ ]
+  row     = 0
+  assert len(parts)>0, "len(parts)==0"
+  while len(collist)<len(oldlist):
+    for part in parts:
+      if row<len(part):
+        collist.append(part[row])
+    row += 1
+  return collist
+  
+
+def partition(list,nparts):
+  """Partion list into n chunks, as evenly sized as possible."""
+  nleft    = len(list)
+  divider  = float(nparts)
+  parts    = [ ]
+  findex   = 0
+  for i in range(0,nparts): # partition recursively
+    nnew   = int(ceil(nleft/divider))
+    lindex = findex + nnew
+    parts.append(list[findex:lindex])
+    nleft   -= nnew
+    divider -= 1
+    findex   = lindex
+    #print nnew
+  return parts
+  
+
+def checkErrors(tree):
+  """Help function to check for asymmetric errors, and detect possible 'Error: closed range without finding crossing'."""
+  varexps = [
+    ('low',  "rLoErr/rErr"),
+    ('up',   "rHiErr/rErr"),
+    ('asym', "abs(rLoErr-rHiErr)/min(rLoErr,rHiErr)"),
+    #('low',  "abs(rLoErr-r)"),
+    #('low',  "abs(rHiErr-r)"),
+  ]
+  marks = [ 2, 10, 100, 1000 ]
+  cutexp = "fit_status==0 && rErr>0" #|| fit_status==300"
+  for name, varexp in varexps:
+    nevts  = tree.Draw(varexp,cutexp,'gOff')
+    if nevts<=0:
+      print warning("Out of %d events none found with %r"%(tree.GetEntries(),cutexp))
+    vector = tree.GetV1()
+    #result = sorted([float(vector[i]) for i in xrange(nevts)])
+    #results.append((name,result))
+    #print result
+    counts = { m: 0 for m in marks }
+    for i in xrange(nevts):
+      x = float(vector[i])
+      for mark in reversed(marks):
+        if x>mark:
+          counts[mark] += 1
+          break
+    for mark in marks:
+      if counts[mark]>0:
+        frac = 100.0*counts[mark]/nevts
+        print warning("Found %s > %2s: %4s/%3s (%.1f%%)"%(varexp,mark,counts[mark],nevts,frac))
+    
+
+def drawBiasTest(filenames,outfilename,rinj=0,text=None,write=False):
+  """Draw bias test."""
+  print '>>> drawBiasTest(%r,%r,rinj=%s)'%(filenames[0],outfilename,rinj)
+  gStyle.SetOptStat(1111)
+  gStyle.SetOptFit(1111)
+  
+  # SETTINGS
+  tname  = 'tree_fit_sb'
+  hname  = 'Toys'
+  htitle = "#mu_{inj.} = %s"%rinj
+  title  = "#mu_{inj.} = %s"%rinj
+  ytitle = "Toys"
+  xtitle = "(#mu_{meas.} - #mu_{inj.}) / #sigma_{#mu}"
+  varexp = "(r-%s)/rErr >> %s"%(rinj,hname)
+  cutexp = "fit_status==0 || fit_status==300" #fit_status==0" # http://cms-analysis.github.io/HiggsAnalysis-CombinedLimit/part3/nonstandard/?#fitting-diagnostics
+  xmin   = -4
+  xmax   = 5
+  nbins  = 30
+  tsize  = 0.050
+  lmargin, rmargin = 0.10, 0.03
+  bmargin, tmargin = 0.12, 0.03
+  
+  # HIST
+  file = None
+  if len(filenames)==1:
+    print ">>> Creating TFile for %r..."%(filenames[0])
+    file     = TFile(filenames[0],'READ')
+    tree     = file.Get(tname)
+  else:
+    print ">>> Creating TChain with %d files..."%(len(filenames))
+    tree     = TChain(tname)
+    for filename in filenames:
+      tree.Add(filename)
+  hist     = TH1F(hname,htitle,nbins,xmin,xmax)
+  #hist.SetDirectory(0)
+  print ">>> Draw bias..."
+  tree.Draw(varexp,cutexp,'gOff')
+  print ">>> Fitting Gaussian..."
+  hist.Fit('gaus',"0")
+  func     = hist.GetFunction('gaus')
+  checkErrors(tree)
+  nevts    = tree.GetEntries()
+  nevts300 = tree.GetEntries("fit_status==300")
+  if nevts300>0:
+    print warning("Found %d/%d (%.1f%%) events with 'fit_status==300'"%(nevts300,nevts,100.0*nevts300/nevts))
+  
+  # WRITE to JSON
+  bias  = func.GetParameter(1)
+  sigma = func.GetParameter(2)
+  berr  = func.GetParError(1) # statistical error in bias
+  if write:
+    print ">>> Writing to JSON..."
+    try:
+      with open('bias.json','r') as jsonfile:
+        data = json.load(jsonfile)
+    except:
+      data = { }
+    with open('bias.json','w') as jsonfile:
+      data[outfilename] = (round(100000*bias)/100000.,round(100000*berr)/100000.,round(100000*sigma)/100000.,hist.GetEntries())
+      json.dump(data,jsonfile,sort_keys=True,indent=2)
+  
+  # DRAW
+  print ">>> Drawing bias test..."
+  ymin, ymax = 0., 1.55*hist.GetMaximum()
+  canvas = TCanvas('canvas','canvas',100,100,900,800)
+  canvas.SetMargin(lmargin,rmargin,bmargin,tmargin) # LRBT
+  #frame = canvas.DrawFrame(xmin,ymin,xmax,ymax)
+  frame = hist
+  frame.GetXaxis().SetTitle(xtitle)
+  frame.GetYaxis().SetTitle(ytitle)
+  frame.GetXaxis().SetTitleSize(tsize)
+  frame.GetYaxis().SetTitleSize(tsize)
+  frame.GetXaxis().SetLabelSize(0.9*tsize)
+  frame.GetYaxis().SetLabelSize(0.9*tsize)
+  frame.GetXaxis().SetTitleOffset(1.04)
+  hist.SetMaximum(ymax)
+  hist.SetLineColor(kBlue+1)
+  hist.SetLineWidth(2)
+  hist.SetMarkerSize(0.8)
+  hist.SetMarkerColor(kBlack)
+  hist.SetMarkerStyle(8)
+  func.SetLineColor(kRed)
+  func.SetLineWidth(2)
+  hist.Draw('E1 SAMES')
+  func.Draw('SAME')
+  
+  # TEXT
+  x = lmargin + (1-lmargin-rmargin)*0.05
+  y = bmargin + (1-tmargin-bmargin)*0.97
+  latex = TLatex()
+  latex.SetTextSize(0.95*tsize)
+  latex.SetTextAlign(13)
+  latex.SetTextFont(62)
+  latex.SetNDC(True)
+  latex.DrawLatex(x,y,title)
+  if text:
+    for i, line in enumerate(text.replace('\\n','\n').split('\n'),1):
+      #print i, line
+      latex.SetTextFont(42)
+      latex.DrawLatex(x,y-(1.3+1.2*i)*tsize,line)
+  
+  # SAVE
+  canvas.SaveAs(outfilename+".png")
+  canvas.SaveAs(outfilename+".pdf")
+  canvas.Close()
+  if file:
+    file.Close()
+  
+
+def plotBiasSummary(jsonname,signal,masses,labels,keyexp,rinj,header=None):
+  """Plot summary of bias test."""
+  gStyle.SetOptStat(False)
+  gStyle.SetOptFit(False)
+  
+  #regexp = re.compile(r"(merge\d+).*([SV]LQ).*M(\d+).*S(\d?)R")
+  biasset = { }
+  with open(jsonname,'r') as jsonfile:
+    data = json.load(jsonfile)
+  for label, _ in labels:
+    biasset.setdefault(label,[ ])
+    for mass in masses:
+      key  = keyexp.replace("$SIGNAL",signal).replace("$LABEL",label).replace("$MASS",str(mass)).replace("$EXPSIG",str(rinj))
+      print ">>> Fetching key %r..."%(key) #data[key]
+      biasset[label].append((data[key][0],data[key][1]))
+    #print ">>>   bias: %s"%(biasset[label])
+  
+  lcolors = [ kBlack, kRed, kBlue, kGreen+1, kOrange+1 ]
+  xwidth  = min(0.5,0.07*len(labels)) # total width of all points combined
+  #biasset = biassets[signal]
+  print ">>> Signal %r"%(signal)
+  graphs = [ ]
+  for il, (label,title) in enumerate(labels,0):
+    print ">>>   label %r"%(label)
+    color = lcolors[il%len(lcolors)]
+    graph = TGraphErrors()
+    graph.SetTitle(title)
+    graph.SetMarkerColor(color)
+    graph.SetLineColor(color)
+    graphs.append(graph)
+    for im, (bias, sigma) in enumerate(biasset[label]):
+      x = 0.5+im + (xwidth*(il-(len(labels)-1)/2.)/(len(labels)-1))
+      print ">>>    x = %4.1f, bias = %.2f +- %.2f"%(x,bias,sigma)
+      graph.SetPoint(im,x,bias)
+      graph.SetPointError(im,0,sigma)
+  
+  title   = "#mu_{inj.} = %s"%rinj
+  text    = 'Vector LQ' if 'VLQ' in signal else 'Scalar LQ' if 'SLQ' in signal else None
+  nbins   = len(masses)
+  pname   = ("bias_summary_%s_S%dR"%(signal,rinj)).replace('S1R','SR')
+  ###ymin, ymax = -1.35, 1.97 # +- 1.0
+  ymin, ymax = -0.60, 0.35 # +- 0.5
+  lmargin, rmargin = 0.128, 0.02
+  bmargin, tmargin = 0.150, 0.02
+  tsize  = 0.045
+  x1, y1 = 0.96-rmargin, 0.98-tmargin
+  canvas  = TCanvas('canvas','canvas',100,100,800,600)
+  canvas.SetMargin(lmargin,rmargin,bmargin,tmargin) # LRBT
+  canvas.SetGrid(1,1)
+  canvas.SetTicks(1,1)
+  #canvas.SetGridx(1)
+  gStyle.SetGridStyle(0)
+  ###frame  = canvas.DrawFrame(0.5,-2,nbins+0.5,+2)
+  frame  = TH1F('frame','frame',3,0,3)
+  frame.GetYaxis().SetRangeUser(ymin,ymax)
+  ###frame.SetMinimum(-2); frame.SetMaximum(2)
+  frame.Draw('AXIS')
+  
+  # MAKE AXIS
+  frame.GetXaxis().SetTitleSize(0.07)
+  frame.GetYaxis().SetTitleSize(0.07)
+  frame.GetXaxis().SetLabelSize(0.08)
+  frame.GetYaxis().SetLabelSize(0.05)
+  frame.GetYaxis().SetTitleOffset(0.88)
+  frame.GetYaxis().SetLabelOffset(0.01)
+  #frame.GetYaxis().CenterTitle(False)
+  frame.GetXaxis().SetTitle("m_{LQ} [GeV]")
+  frame.GetYaxis().SetTitle("Fitted bias #pm #sigma_{bias}")
+  frame.GetXaxis().SetNdivisions(304)
+  frame.GetYaxis().SetNdivisions(508)
+  for i, mass in enumerate(masses):
+    index = frame.GetXaxis().FindBin(i)
+    frame.GetXaxis().SetBinLabel(index,str(mass))
+  ###frame.GetXaxis().ChangeLabel(2,-1,-1,-1,-1,-1,"600")
+  ###frame.GetXaxis().ChangeLabel(-2,-1,-1,-1,-1,-1,"1300")
+  ###frame.GetXaxis().LabelsOption("h")
+  canvas.Modified()
+  canvas.Update()
+  
+  # TLINE
+  lines  = [ ]
+  ylines = [-0.4,-0.2,0] #[-1,0,1]
+  for y in ylines:
+    line = TLine(0,y,3,y)
+    line.SetLineStyle(2 if y==0 else 3)
+    line.SetLineColor(kGray+(3 if y==0 else 1))
+    line.Draw()
+    lines.append(line)
+  
+  # LEGEND
+  nlines = len(graphs)+(1 if header else 0)
+  lsplit = nlines>=4 #and False
+  nrows  = (1 if header else 0) + ceil(nlines/2.) if lsplit else nlines
+  width  = (0.40 if lsplit else 0.27)
+  height = 1.20*tsize*nrows
+  margin = (0.10 if lsplit else 0.05)/width
+  legend = TLegend(x1,y1,x1-width,y1-height)
+  legend.SetMargin(margin)
+  legend.SetFillStyle(0)
+  legend.SetBorderSize(0)
+  legend.SetTextSize(tsize)
+  if header:
+    legend.SetTextFont(62)
+    legend.SetHeader(header)
+  legend.SetTextFont(42)
+  if lsplit:
+    legend.SetNColumns(2)
+    legend.SetColumnSeparation(0.02)
+  legend.Draw()
+  
+  # TEXT
+  x = lmargin + (1-lmargin-rmargin)*0.06
+  y = bmargin + (1-tmargin-bmargin)*0.96
+  latex = TLatex()
+  latex.SetTextSize(1.1*tsize)
+  latex.SetTextAlign(13)
+  latex.SetTextFont(62)
+  latex.SetNDC(True)
+  latex.DrawLatex(x,y,title)
+  if text:
+    latex.SetTextFont(42)
+    latex.DrawLatex(x,y-2*tsize,text)
+  
+  # DRAW
+  for graph in graphs:
+    graph.SetMarkerStyle(8)
+    graph.SetMarkerSize(1.2)
+    graph.SetLineWidth(2)
+    graph.Draw("PLSAME")
+  for graph in columnize(graphs,2):
+    legend.AddEntry(graph,graph.GetTitle(),'LEP')
+  #frame.Draw('SAMEAXIS')
+  legend.Draw()
+  
+  # SAVE & CLOSE
+  canvas.SaveAs(pname+'.png')
+  canvas.SaveAs(pname+'.pdf')
+  canvas.Close()
+  gROOT.Delete(frame.GetName())
+  
+
+
+def main():
+    
+    # PLOT BIAS TEST
+    filenames   = args.filenames
+    outfilename = args.outfile
+    rinj        = args.rinj
+    text        = args.text.replace("GeV, #","GeV,\n#")
+    
+    if filenames[0].endswith('.json'):
+      # PLOT BIAS SUMMARY
+      jsonname = 'bias/bias.json'
+      signals = ['SLQ','VLQ']
+      expsigs = [0,1,2,3,]
+      masses  = { 'SLQ': [600,1000,1400], 'VLQ': [500,1100,1400], }
+      header  = "Bin merge threshold" # (max. unc.)
+      for signal in signals:
+        for rinj in expsigs:
+          key    = "../plots/biastest_combined_Run2_$LABEL_$SIGNAL_noBBBToys-M$MASS-S$EXPSIGR"
+          thres  = [10,15,20,25,30] # [10,15,20,25] if rinj<=1 else [10,15,20,25,30]
+          labels = [("merge%s"%x, "%d%%"%x) for x in thres]
+          if rinj==1:
+            key = key.replace('$EXPSIG','')
+          plotBiasSummary(jsonname,signal,masses[signal],labels,key,rinj,header=header)
+    else:
+      drawBiasTest(filenames,outfilename,rinj,text,write=True)
+    
+
+
+if __name__ == '__main__':
+    main()
+    
+
