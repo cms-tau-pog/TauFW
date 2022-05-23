c33bbc9702cf1e8d16c6f69c7e839e0124915c0a
IzaakWN <iwn_@hotmail.com>
2022-05-23 16:58:52 +0200

    debug

--- Fitter/scripts/plotChannelCompatibility.py
+++ Fitter/scripts/plotChannelCompatibility.py
@@ -16,8 +16,27 @@ gStyle.SetOptStat(0) # no stat. box
 gStyle.SetEndErrorSize(6) # size of bars at the end of error bars
 
 
-def getPOIs(fname,poi='r',chandict={ }):
+def rmprefix(var,poiname='r'):
+  """Help function to remove the ChannelCompatibilityCheck prefix."""
+  #vname = lambda v: v.GetName().replace(prefix,'') # help function
+  prefix = "_ChannelCompatibilityCheck_%s_"%(poiname)
+  return var.GetName().replace(prefix,'')
+
+
+def channelkey(var,keys,poiname='r'):
+  """Sort key to sort according to the order in the dictionary."""
+  #lambda v: keys.index(rmprefix(v)) if rmprefix(v) in keys else len(keys))
+  vname = rmprefix(var,poiname)
+  return keys.index(vname) if vname in keys else len(keys)
+  
+
+def getPOIs(fname,**kwargs):
   """Get POI from ChannelCompatibilityCheck file."""
+  poiname  = kwargs.get('poi',      None ) # string
+  chandict = kwargs.get('chandict', { }  ) # dictionary for channel POIs
+  verb     = kwargs.get('verb',     0    )
+  if verb>=1:
+    print ">>> getPOIs(%r)"%(fname)
   
   # GET FITS
   file = TFile.Open(fname,'READ')
@@ -29,44 +48,43 @@ def getPOIs(fname,poi='r',chandict={ }):
     return
   
   # GET MAIN POI
-  poi = fit_nom.floatParsFinal().find(poiname)
+  prefix = "_ChannelCompatibilityCheck_%s_"%(poiname)
+  poi = fit_nom.floatParsFinal().find(poiname) # RooRealVar object
   if not poi:
     print "Nominal fit does not contain parameter of interest %s"%(poiname)
     return
-  if rmin==None:
-    rmin = poi.getMin() #min(poi.getMin(),rmin)
-  if rmax==None:
-    rmax = poi.getMax() #max(poi.getMax(),rmax)
   
   # GET CHANNEL POIs
   pois_chan = [ ]
-  prefix = "_ChannelCompatibilityCheck_%s_"%(poiname)
   iter = fit_alt.floatParsFinal().createIterator()
   var = iter.Next()
-  vname = lambda v: v.GetName().replace(prefix,'') # help function
   while var:
     if prefix in var.GetName():
-      channel = vname(var)
+      varname = var.GetName()
+      channel = rmprefix(var,poiname)
+      var.SetName(channel)
       if channel in chandict:
         channel = chandict[channel]
       var.SetTitle(channel)
-      print ">>> Found channel POI %r -> %r"%(var.GetName(),channel)
+      print ">>> Found channel POI %r -> %r"%(varname,channel)
       pois_chan.append(var)
     var = iter.Next()
   
   file.Close()
-  return poi, pois_chan
+  return poi, pois_chan # RooRealVar, [RooRealVar,...]
+  
 
-
-def plotChannelCompatibility(fname,outname,poi='r',rmin=None,rmax=None,**kwargs):
+def plotChannelCompatibility(fname,outname,**kwargs):
   # Adapted from
   #   https://github.com/cms-analysis/HiggsAnalysis-CombinedLimit/blob/master/test/plotting/cccPlot.cxx
-  poiname  = poi
-  xtitle   = kwargs.get('xtitle',  "r"     ) #" #sigma/#sigma_{SM}"
+  poiname  = kwargs.get('poi',     None    ) # string
+  xtitle   = kwargs.get('xtitle',  None    ) #" #sigma/#sigma_{SM}"
   chandict = kwargs.get('chandict',{ }     ) # dictionary for channel POIs
   others   = kwargs.get('others',  [ ]     )
   texts    = kwargs.get('text',    [ ]     ) or [ ]
   exts     = kwargs.get('ext',     ['png'] )
+  rmin     = kwargs.get('rmin',    None    )
+  rmax     = kwargs.get('rmax',    None    )
   verb     = kwargs.get('verb',    0       )
   if not isinstance(exts,list):
     exts = list(exts)
@@ -74,11 +92,11 @@ def plotChannelCompatibility(fname,outname,poi='r',rmin=None,rmax=None,**kwargs)
     texts = texts.split('\\n') if '\\n' in texts else texts.split('\n')
   
   # GET POIs
-  poi, pois_chan = getPOIs(fname,poi=poiname,chandict=chandict)
+  poi, pois_chan = getPOIs(fname,**kwargs) # RooRealVar, [RooRealVar,...]
   if others:
     for ofname in others:
       pois_chan1 = pois_chan[:] # so list does not increase during iteration
-      poi2, pois_chan2 = getPOIs(ofname,poi=poiname,chandict=chandict)
+      poi2, pois_chan2 = getPOIs(ofname,**kwargs)
       if (poi.getVal()!=poi2.getVal() or poi.getAsymErrorLo()!=poi2.getAsymErrorLo() or poi.getAsymErrorHi()!=poi2.getAsymErrorHi()):
         print ">>> WARNING! POI results for other file %s (%s = %7.2f %+4.2f %+4.2f) does not match main file %s (%s = %7.2f %+4.2f %+4.2f)"%(
           ofname,poi2.GetTitle(),poi2.getVal(),poi2.getAsymErrorLo(),poi2.getAsymErrorHi(),
@@ -90,32 +108,52 @@ def plotChannelCompatibility(fname,outname,poi='r',rmin=None,rmax=None,**kwargs)
   
   # SORT
   if chandict: # reorder according to ordered channel dictionary
-    keys = chandict.keys()
-    pois_chan.sort(key=lambda v: keys.index(vname(v)) if vname(v) in keys else len(keys))
+    keys = chandict.keys() # ordered keys
+    pois_chan.sort(key=lambda v: channelkey(v,keys,poiname))
+  poi.SetTitle('Combined')
+  pois_chan.append(poi) # add main POI
   
   # CREATE POINTS
-  if verb>=1:
-    print ">>> xtitle=%s"%(xtitle)
-    print ">>> rmin=%s, rmax=%s"%(rmin,rmax)
-  print ">>> %7.2f %+4.2f %+4.2f %s"%(poi.getVal(),poi.getAsymErrorLo(),poi.getAsymErrorHi(),poi.GetTitle())
+  rmin_ = poi.getMin() #min(poi.getMin(),rmin)
+  rmax_ = poi.getMax() #max(poi.getMax(),rmax)
+  print ">>> %7.2f %+4.2f %+4.2f %s (%r)"%(poi.getVal(),poi.getAsymErrorHi(),poi.getAsymErrorLo(),poi.GetName(),poi.GetTitle())
   nchans = len(pois_chan)
-  frame = TH2F('frame',";Best fit %s;"%(xtitle),1,rmin,rmax,nchans,0,nchans)
   graph = TGraphAsymmErrors(nchans)
   for i, poi_chan in enumerate(pois_chan):
+    varname = poi_chan.GetName() #rmprefix(poi_chan,poiname)
     channel = poi_chan.GetTitle()
     yval = nchans-i-0.5
     xval = poi_chan.getVal()
-    errup, errdn = poi_chan.getAsymErrorLo(), poi_chan.getAsymErrorHi()
-    print ">>> %7.2f %+4.2f %+4.2f %s"%(xval,errup,errdn,channel) #poi_chan.GetName()
+    errdn, errup = -poi_chan.getAsymErrorLo(), poi_chan.getAsymErrorHi()
+    print ">>> %7.2f %+4.2f %+4.2f %s (%r)"%(xval,errup,-errdn,varname,channel) #poi_chan.GetName()
     graph.SetPoint(i,xval,yval)
-    graph.SetPointError(i,-errup,errdn,0,0)
+    graph.SetPointError(i,errdn,errup,0,0)
+    if rmin_>xval-errdn:
+      rmin_ = 1.05*(xval-errdn)-0.05*rmax_ # add 5% margin
+    if rmax_<xval+errup:
+      rmax_ = 1.05*(xval+errup)-0.05*rmin_ # add 5% margin
+  
+  # CREATE FRAME
+  if rmin==None:
+    rmin = rmin_ #min(poi.getMin(),rmin)
+  if rmax==None:
+    rmax = rmax_ #max(poi.getMax(),rmax)
+  if xtitle==None:
+    xtitle = chandict.get(poiname,poiname)
+  if verb>=1:
+    print ">>> xtitle=%r, rmin=%s, rmax=%s"%(xtitle,rmin,rmax)
+  frame = TH2F('frame',";Best fit %s;"%(xtitle),1,rmin,rmax,nchans,0,nchans)
+  for i, poi_chan in enumerate(pois_chan):
+    channel = poi_chan.GetTitle()
     frame.GetYaxis().SetBinLabel(nchans-i,channel)
   
   # PREPARE CANVAS
-  tsize = 0.045
-  canvas = TCanvas('canvas')
-  canvas.SetMargin(0.2,0.02,0.15,0.02) # LRBT
+  tsize  = 0.045
+  bmarg  = 0.15
+  canvas = TCanvas("canvas","Pulls",900,150+nchans*80)
+  canvas.SetMargin(0.2,0.02,bmarg,0.02) # LRBT
   canvas.SetGridx(1)
+  canvas.SetTicks(1,1)
   graph.SetLineColor(kRed)
   graph.SetLineWidth(3)
   graph.SetMarkerStyle(21)
@@ -142,15 +180,15 @@ def plotChannelCompatibility(fname,outname,poi='r',rmin=None,rmax=None,**kwargs)
   if texts:
     latex = TLatex()
     latex.SetTextSize(tsize)
-    latex.SetTextAlign(33)
+    latex.SetTextAlign(30)
     latex.SetTextFont(42)
     #latex.SetTextColor(kRed)
     latex.SetNDC(True)
-    x, y = 0.96, 0.96
+    x, y = 0.96, bmarg+0.04
     for i, line in enumerate(texts):
       y -= 1.15*i*tsize
       if verb>=1:
-        print ">>> Extra text: x=%s y=%s, line=%s"%(x,y,line)
+        print ">>> Extra text: x=%s y=%s, line=%r"%(x,y,line)
       latex.DrawLatex(x,y,line)
   
   # STORE
@@ -173,7 +211,7 @@ def main(args):
   dictfname  = args.translate
   chantitles = args.chantitles
   rmin, rmax = args.rMin, args.rMax
-  verbosity  = args.verbosity
+  verbosity  = args.verbosity+4
   chandict   = OrderedDict()
   if dictfname:
     if verbosity>=2:
@@ -181,11 +219,10 @@ def main(args):
     with open(translate,'r') as dfile:
       chandict = json.load(dfile,object_pairs_hook=OrderedDict)
   for channel in chantitles:
-    if verbosity>=2:
-      print ">>> Parsing channel title %r"%(channel)
     channel, chantitle = channel.split('=',1) if '=' in channel else (channel,channel)
     chandict[channel] = chantitle or channel
-    print ">>> %r -> %r"%(channel,chandict[channel])
+    if verbosity>=2:
+      print ">>> Parsed channel title %r -> %r"%(channel,chandict[channel])
   for fname in filenames:
     for mass in masses:
       for name in names:
@@ -210,7 +247,7 @@ if __name__ == "__main__":
   parser.add_argument(     "--rMin",      type=float, help="Minimum of POI range for x axis")
   parser.add_argument(     "--rMax",      type=float, help="Maximum of POI range for x axis")
   parser.add_argument('-E',"--text",      help="extra text")
-  parser.add_argument('-p',"--title",     default='r', help="Title of POI for title of x axis")
+  parser.add_argument('-p',"--title",     help="Title of POI for title of x axis")
   parser.add_argument('-P',"--poi",       default='r',help="Parameter of interest")
   parser.add_argument('-t',"--tag",       default='t',help="Tag for output file")
   parser.add_argument('-c',"--chantitles",nargs='+',default=[],help="titles for channels")
