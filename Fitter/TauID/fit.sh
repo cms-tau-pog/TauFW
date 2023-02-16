de0ce057c0080ebd8c7b21325ed3960023fd93ef
IzaakWN <iwn_@hotmail.com>
2023-02-16 16:04:24 +0100

    add logs

--- Fitter/TauID/fit.sh
+++ Fitter/TauID/fit.sh
@@ -42,7 +42,7 @@ fi
 # MAIN FUNCTIONALITY
 function main {
   #header "main"
-  ensuredir "output"
+  ensuredir "output" "log"
   for era in $ERAS; do
     for wp in $WPS; do
       #header "era=$era, WP=$wp"
@@ -58,18 +58,20 @@ function main {
         INPUTS_MM="input/ztt_tid_mvis_mm-${era}.inputs.root"
         [ ! -f $INPUTS_MT ] && echo ">>> $INPUTS_MT not found!!" && continue
         [ ! -f $INPUTS_MM ] && echo ">>> $INPUTS_MM not found!!" && continue
+        LOG="log/ztt_${LABEL}.log"
+        echo `date` > $LOG
         
         # HARVEST DATACARDS => txt datacards
         if [[ $DOHARV -gt 0 ]]; then
-          peval "python harvestcards.py $INPUTS_MT $INPUTS_MM -y $era -w $wp -t _${bin} -v $VERB"
+          peval "python harvestcards.py $INPUTS_MT $INPUTS_MM -y $era -w $wp -t _${bin} -v $VERB" | tee -a $LOG
         fi
         
         # COMBINE FIT WITH TENSOR FLOW
-        #peval "text2hdf5.py $CARDTXT -m 90 -o $CARDHDF"
-        #peval "combinetf.py $CARDHDF  --binByBinStat --output output/fit_${LABEL}.root --saveHists"
+        #peval "text2hdf5.py $CARDTXT -m 90 -o $CARDHDF" | tee -a ../$LOG
+        #peval "combinetf.py $CARDHDF  --binByBinStat --output output/fit_${LABEL}.root --saveHists" | tee -a ../$LOG
         
         # IMPACTS
-        #combinetf.py $CARDHDF --binByBinStat --output root/impacts_${LABEL}_impact.root --doImpacts
+        #combinetf.py $CARDHDF --binByBinStat --output root/impacts_${LABEL}_impact.root --doImpacts | tee -a ../$LOG
         
       done
     done
