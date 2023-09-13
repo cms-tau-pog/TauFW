e6562811784af89128e4a96bc17f85caa8c74866
IzaakWN <iwn_@hotmail.com>
2023-09-13 19:39:26 +0200

    add Run3

--- Plotter/python/plot/CMSStyle.py
+++ Plotter/python/plot/CMSStyle.py
@@ -48,17 +48,18 @@ relExtraDY     = 1.2
 drawLogo       = False
 outOfFrame     = False
 lumi_dict      = {
-  '7':      5.1,    '2016': 36.3, 'UL2016_preVFP':  19.5, # actually 19.5, update after reprocessing with new JSON
-  '8':      19.7,   '2017': 41.5, 'UL2016_postVFP': 16.8, # actually 16.8, update after reprocessing with new JSON
-  '2012':   19.7,   '2018': 59.7, 'UL2016': 36.3, # actually 19.5+16.8=36.3
-  'Run2':   138,                  'UL2017': 41.5, # actually 41.5
-  'Phase2': 3000,                 'UL2018': 59.8, # actually 59.8
+  '7':      5.1,  '2016': 36.3, 'UL2016_preVFP':  19.5, # actually 19.5, update after reprocessing with new JSON
+  '8':      19.7, '2017': 41.5, 'UL2016_postVFP': 16.8, # actually 16.8, update after reprocessing with new JSON
+  '2012':   19.7, '2018': 59.7, 'UL2016': 36.3, # actually 19.5+16.8=36.3
+  'Run2':   138,                'UL2017': 41.5, # actually 41.5
+  'Phase2': 3000,               'UL2018': 59.8, # actually 59.8
 }
 cme_dict       = {
-  '7':      7,   '2016': 13,
-  '8':      8,   '2017': 13,
-  '2012':   8,   '2018': 13,
-  'Run2':   13,
+  '7':      7,    '2016': 13,
+  '8':      8,    '2017': 13,
+  '2012':   8,    '2018': 13,
+  'Run2':   13,   '2022': 13,
+  'Run3':   13.7, '2023': 13,
   'Phase2': 14,
 }
 era_dict       = {
