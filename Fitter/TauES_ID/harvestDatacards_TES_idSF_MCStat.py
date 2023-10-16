d25c04bbbc3645c2e94781dbf654b8bb0484fe1d
oponcet <oceane.poncet@cern.ch>
2023-10-16 10:46:59 +0200

    fix bug for WJ SF, just adding []

--- Fitter/TauES_ID/harvestDatacards_TES_idSF_MCStat.py
+++ Fitter/TauES_ID/harvestDatacards_TES_idSF_MCStat.py
@@ -125,8 +125,8 @@ def harvest(setup, year, obs, **kwargs):
         if not "norm_wj" in setup["systematics"]:
           print("W+Jets SF as a free parameter ")
           sf_W = "sf_W_%s"%(region)
-          harvester.cp().process("W").AddSyst(harvester, sf_W,'rateParam', SystMap()(1.00))
-          print(">>>Add sf_W : %s" %(sf_W))
+          harvester.cp().process(['W']).AddSyst(harvester, sf_W,'rateParam', SystMap()(1.00))
+          #print(">>>Add sf_W : %s" %(sf_W))
 
         # Add DY cross section as a free parameter. Don't forgot to add Zmm CR !
         if not "xsec_dy" in setup["systematics"]:
