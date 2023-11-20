4f81e906dbb6c1d167b807b1941436943a064b7c
IzaakWN <iwn_@hotmail.com>
2023-11-20 14:32:27 +0100

    add histograms

--- PicoProducer/python/analysis/gmin2/ModuleGen_gmin2.py
+++ PicoProducer/python/analysis/gmin2/ModuleGen_gmin2.py
@@ -110,9 +110,25 @@ class ModuleGen_gmin2(Module):
     #self.out.addHist('nfsr_os',   "Number of FSR photons of OS leptons;Number of FSR photons of positively charged lepton;Number of FSR photons of negatively charged lepton",
     #  10,0,10,10,0,10) # to study FSR
     self.out.addHist('nfsr2D',    "Number of FSR photons of two leading lepton;Number of FSR photons from leading lepton;Number of FSR photons from subleading lepton",
-                     10,0,10,10,0,10) # to study FSR
-    self.out.addHist('ncopies',   "Number of copies of two leading lepton",10,0,10) # to study FSR
-    self.out.addHist('pid_lep1D', "Final state of two leading leptons",6,0,6,xlabs=pidlabs2,lsize=0.045)
+                     10,0,10,10,0,10,option='COLZ TEXT44') # to study FSR
+    self.out.addHist('dphi_fsr',  "#Delta#eta between lepton before and after FSR",       100,0,4) # to study FSR
+    self.out.addHist('dphi_fsr1', "#Delta#eta between lepton before and after single FSR",100,0,4) # to study FSR
+    self.out.addHist('deta_fsr',  "#Delta#eta between lepton before and after FSR",       100,0,5) # to study FSR
+    self.out.addHist('deta_fsr1', "#Delta#eta between lepton before and after single FSR",100,0,5) # to study FSR
+    self.out.addHist('dR_fsr',    "#DeltaR between lepton before and after FSR",          100,0,5) # to study FSR
+    self.out.addHist('dR_fsr1',   "#DeltaR between lepton before and after single FSR",   100,0,5) # to study FSR
+    self.out.addHist('dpt_fsr',   "(p_{T}^{after}-p_{T}^{before})/p_{T}^{before} between lepton before and after FSR",       100,-1.4,0.7) # to study FSR
+    self.out.addHist('dpt_fsr1',  "(p_{T}^{after}-p_{T}^{before})/p_{T}^{before} between lepton before and after single FSR",100,-1.4,0.7) # to study FSR
+    self.out.addHist('dphi_deta_fsr', "Before and after FSR;#Delta#eta;#Delta#phi",       100,0,4,100,0,4) # to study FSR
+    self.out.addHist('dphi_deta_fsr1',"Before and after single FSR;#Delta#eta;#Delta#phi",100,0,4,100,0,4) # to study FSR
+    self.out.addHist('pt_pt_fsr',     "Lepton pT before and after FSR;Lepton p_{T} before FSR [GeV];Lepton pT after FSR [GeV];",       40,0,120,40,0,120) # to study FSR
+    self.out.addHist('pt_pt_fsr1',    "Lepton pT before and after single FSR;Lepton p_{T} before FSR [GeV];Lepton pT after FSR [GeV];",40,0,120,40,0,120) # to study FSR
+    self.out.addHist('dpt_dR_fsr',    "Before and after FSR;#DeltaR;(p_{T}^{after}-p_{T}^{before})/p_{T}^{before}",       100,-1.4,0.7,100,0,5) # to study FSR
+    self.out.addHist('dpt_dR_fsr1',   "Before and after single FSR;#DeltaR;(p_{T}^{after}-p_{T}^{before})/p_{T}^{before}",100,-1.4,0.7,100,0,5) # to study FSR
+    self.out.addHist('dpt_pt_fsr',    "Lepton pT before and after FSR;Lepton p_{T} before FSR [GeV];(p_{T}^{after}-p_{T}^{before})/p_{T}^{before};",       40,0,120,100,-1.4,0.7) # to study FSR
+    self.out.addHist('dpt_pt_fsr1',   "Lepton pT before and after single FSR;Lepton p_{T} before FSR [GeV];(p_{T}^{after}-p_{T}^{before})/p_{T}^{before};",40,0,120,100,-1.4,0.7) # to study FSR
+    self.out.addHist('ncopies',     "Number of copies of two leading lepton",10,0,10) # to study FSR
+    self.out.addHist('pid_lep1D',   "Final state of two leading leptons",6,0,6,xlabs=pidlabs2,lsize=0.045)
     self.out.addHist('abspid_lep2D',"Final state of two leading leptons;Leading lepton;Subleading lepton",4,0,4,4,0,4,
                      xlabs=abspidlabs,ylabs=abspidlabs,lsize=0.045,option='COLZ TEXT44')
     self.out.addHist('pid_lep2D',"Final state of two leading leptons;Leading lepton;Subleading lepton",7,0,7,7,0,7,
@@ -137,6 +153,8 @@ class ModuleGen_gmin2(Module):
     self.out.addBranch('ptfirst_lep2',   'f', title="pT of first copy of subleading lepton")
     self.out.addBranch('eta_lep1',       'f', title="eta of leading lepton")
     self.out.addBranch('eta_lep2',       'f', title="eta of subleading lepton")
+    self.out.addBranch('etafirst_lep1',  'f', title="eta of first copy leading lepton")
+    self.out.addBranch('etafirst_lep2',  'f', title="eta of first copy subleading lepton")
     self.out.addBranch('phi_lep1',       'f', title="phi of leading lepton")
     self.out.addBranch('phi_lep2',       'f', title="phi of subleading lepton")
     self.out.addBranch('phifirst_lep1',  'f', title="phi of first copy leading lepton")
@@ -233,20 +251,44 @@ class ModuleGen_gmin2(Module):
       lastlep = getlastcopy(photon.mother,genparts) # get last copy of tau (after all FSR)
       lastlep.photons.append(photon)
     
-    #### SANITY CHECKS for charge balance
-    ###leptons = elecs+muons+taus
-    ###for lepton in leptons:
-    ###  if lepton.pdgId>0: # positive charge
-    ###    self.out.fill('pt_pos',lepton.pt)
-    ###    self.out.fill('nfsr_pos',len(lepton.photons))
-    ###  else: # negatively charged
-    ###    self.out.fill('pt_neg',lepton.pt)
-    ###    self.out.fill('nfsr_neg',len(lepton.photons))
-    ###  poslep = [l for l in leptons if l.pdgId>0]
-    ###  neglep = [l for l in leptons if l.pdgId<0]
-    ###  if len(poslep)>=1 and len(neglep)>=1:
-    ###    self.out.fill('pt_os',poslep[0].pt,neglep[0].pt)
-    ###    self.out.fill('nfsr_os',len(poslep[0].photons),len(neglep[0].photons))
+    # SANITY CHECKS for charge balance
+    leptons = elecs+muons+taus
+    for lepton in leptons:
+      if len(lepton.photons)>=1:
+        lepton0 = lepton.copies[0]
+        assert lepton!=lepton0
+        dphi = deltaPhi(lepton0.phi,lepton.phi)
+        deta = abs(lepton0.eta-lepton.eta)
+        dR   = sqrt(dphi*dphi+deta*deta)
+        dpt  = (lepton.pt-lepton0.pt)/lepton0.pt if lepton0.pt>0 else 0
+        self.out.fill('dphi_fsr',     dphi)
+        self.out.fill('deta_fsr',     deta)
+        self.out.fill('dR_fsr',       dR)
+        self.out.fill('dpt_fsr',      dpt)
+        self.out.fill('dphi_deta_fsr',deta,dphi)
+        self.out.fill('pt_pt_fsr',    lepton0.pt,lepton.pt)
+        self.out.fill('dpt_dR_fsr',   dR,dpt)
+        self.out.fill('dpt_pt_fsr',   lepton0.pt,dpt)
+        if len(lepton.photons)==1: # single FSR for this lepton
+          self.out.fill('dphi_fsr1',     dphi)
+          self.out.fill('deta_fsr1',     deta)
+          self.out.fill('dR_fsr1',       dR)
+          self.out.fill('dpt_fsr1',      dpt)
+          self.out.fill('dphi_deta_fsr1',deta,dphi)
+          self.out.fill('pt_pt_fsr1',    lepton0.pt,lepton.pt)
+          self.out.fill('dpt_dR_fsr1',   dR,dpt)
+          self.out.fill('dpt_pt_fsr1',   lepton0.pt,dpt)
+      ###if lepton.pdgId>0: # positive charge
+      ###  self.out.fill('pt_pos',lepton.pt)
+      ###  self.out.fill('nfsr_pos',len(lepton.photons))
+      ###else: # negatively charged
+      ###  self.out.fill('pt_neg',lepton.pt)
+      ###  self.out.fill('nfsr_neg',len(lepton.photons))
+      ###poslep = [l for l in leptons if l.pdgId>0]
+      ###neglep = [l for l in leptons if l.pdgId<0]
+      ###if len(poslep)>=1 and len(neglep)>=1:
+      ###  self.out.fill('pt_os',poslep[0].pt,neglep[0].pt)
+      ###  self.out.fill('nfsr_os',len(poslep[0].photons),len(neglep[0].photons))
     
     # SORT LEPTONS BY PT
     leptons = elecs+muons+taus
@@ -275,6 +317,8 @@ class ModuleGen_gmin2(Module):
       self.out.ptfirst_lep2[0]  = leptons[1].copies[0].pt
       self.out.eta_lep1[0]      = leptons[0].eta
       self.out.eta_lep2[0]      = leptons[1].eta
+      self.out.etafirst_lep1[0] = leptons[0].copies[0].eta
+      self.out.etafirst_lep2[0] = leptons[1].copies[0].eta
       self.out.phi_lep1[0]      = leptons[0].phi
       self.out.phi_lep2[0]      = leptons[1].phi
       self.out.phifirst_lep1[0] = leptons[0].copies[0].phi
@@ -298,6 +342,8 @@ class ModuleGen_gmin2(Module):
       self.out.ptfirst_lep2[0]  = -1
       self.out.eta_lep1[0]      = leptons[0].eta
       self.out.eta_lep2[0]      = -9
+      self.out.etafirst_lep1[0] = leptons[0].copies[0].eta
+      self.out.etafirst_lep2[0] = -9
       self.out.phi_lep1[0]      = leptons[0].phi
       self.out.phi_lep2[0]      = -9
       self.out.phifirst_lep1[0] = leptons[0].copies[0].phi
@@ -321,6 +367,8 @@ class ModuleGen_gmin2(Module):
       self.out.ptfirst_lep2[0]  = -1
       self.out.eta_lep1[0]      = -9
       self.out.eta_lep2[0]      = -9
+      self.out.etafirst_lep1[0] = -9
+      self.out.etafirst_lep2[0] = -9
       self.out.phi_lep1[0]      = -9
       self.out.phi_lep2[0]      = -9
       self.out.phifirst_lep1[0] = -9
