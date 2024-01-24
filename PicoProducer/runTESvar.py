26f44750c7d660017f554e144a6cdd44c620764e
oponcet <oceane.poncet@cern.ch>
2024-01-24 13:30:38 +0100

    change variation values

--- PicoProducer/runTESvar.py
+++ PicoProducer/runTESvar.py
@@ -3,16 +3,18 @@ import sys
 
 
 job = sys.argv[1] #submit-resubmit-hadd-clean 
-tes_start = 0.900
+year = sys.argv[2] # year
+tes_start = 0.800
 # tes_stop = 0.972
 # tes_step = 0.002
-tes_stop = 0.958
+tes_stop = 0.848
 tes_step = 0.002
+#year = "2022_postEE"
 
 for tes_val in [tes_start + i * tes_step for i in range(int((tes_stop - tes_start) / tes_step) + 1)]:
     tes = "TES" + "{:.3f}".format(tes_val).replace(".", "p")
     #command = "pico.py %s -y UL2018_v10 -c mutau_%s -s DY - " %(job,tes)
-    command = "pico.py %s -c mutau_%s -y 2022_postEE -E jec=False --dtype mc -s DY" %(job,tes)
+    command = "pico.py %s -c mutau_%s -y %s -E jec=False --dtype mc -s DY" %(job,tes, year)
     print(command)
     os.system(command)
 
