bbd503c9e661ed2a361cbff39a0689b60b0d7d64
IzaakWN <iwn_@hotmail.com>
2024-04-22 19:29:26 +0200

    resolve conflicts; fix lxplus HTCondor environment issue; add possibility for CMSSW singularity

--- PicoProducer/python/batch/submit_HTCondor.sh
+++ PicoProducer/python/batch/submit_HTCondor.sh
@@ -2,39 +2,76 @@
 ## Script to run on a HTCondor batch system
 
 # START
-START=`date +%s`
-echo "Job start at `date`"
-echo "Running job on machine `uname -a`, host $HOSTNAME"
-function peval { echo ">>> $@"; eval "$@"; }
+START="$(date +%s)"
+echo "Job starts at $(date)"
+echo "Running job on machine $(uname -a), host $HOSTNAME"
 
-# SETTING
-TASKCMD="$@"
-WORKDIR="$PWD"
-printf '=%.0s' `seq 60`; echo
-echo "\$PWD=$PWD"
-echo "\$JOBID=$JOBID"
-echo "\$TASKID=$TASKID"
-echo "\$HOSTNAME=$HOSTNAME"
-echo "\$TASKCMD=$TASKCMD"
-echo "\$WORKDIR=$WORKDIR"
-#printf '=%.0s' `seq 60`; echo
-#env
-#printf '=%.0s' `seq 60`; echo
+# SETTINGS: store in file for reuse
+cat << EOF > setenv.sh
+function peval { echo ">>> \$@"; eval "\$@"; }
+function pbar { printf '=%.0s' \$(seq \${1:-70}); echo; }
+VERB=${VERB:-0} # verbosity level for debugging
+START=$START
+JOBID=$JOBID
+TASKID=$TASKID
+HOSTNAME=$HOSTNAME
+PWD=$PWD
+WORKDIR=$PWD
+CONTAINER='$CONTAINER' # to set OS environment with container (Singularity, e.g. "cmssw-el7")
+CMSSW_BASE=$CMSSW_BASE # to set CMSSW environment
+TASKCMD='$@'
+EOF
+source setenv.sh # set environment
+pbar
+peval 'tail -n +3 setenv.sh | while read line; do echo "\$$line"; done'
+pbar
+[ $VERB -ge 1 ] && { peval "env"; pbar; } # print out environment for debugging
 
-# ENVIRONMENT
-if [ ! -z "$CMSSW_BASE" -a -d "$CMSSW_BASE/src" ]; then
-  peval "cd $CMSSW_BASE/src"
-  peval 'eval `scramv1 runtime -sh`'
-  peval "cd $WORKDIR"
+# OS ENVIRONMENT with container (Singularity)
+# https://cms-sw.github.io/singularity.html
+# https://apptainer.org/docs/user/main/environment_and_metadata.html
+if [ ! -z "$CONTAINER" ]; then # if $CONTAINER is set
+  echo ">>> Setting OS environment with container '$CONTAINER'..."
+  peval "$CONTAINER" # setup container (Singularity, e.g. "cmssw-el7")
+  peval "source setenv.sh" # set environment again (incl. functions) after Singularity
+  [ $VERB -ge 1 ] && { peval "env"; pbar; } # print out environment for debugging
+fi
+
+# CMSSW ENVIRONMENT
+if [ -z "$CMSSW_BASE" ]; then # $CMSSW_BASE is not set
+  echo ">>> WARNING! CMSSW_BASE was not defined!"
+  # NOTE: HTCondor on lxplus does not seem to transfer environment as expected...
+  # Guess CMSSW_BASE path from $CMSSW_BASE/src/TauFW/PicoProducer/python/batch/submit_HTCondor.sh
+  SCRIPT="$(echo $TASKCMD | awk '{ print $2 }')" # assume `[COMMAND] [SCRIPT] [OPTIONS]`
+  CMSSW_BASE=$(realpath $(dirname "${SCRIPT}")/../../../../..)
+  echo ">>> Guessing CMSSW_BASE=$CMSSW_BASE based on SCRIPT=$SCRIPT"
+fi
+if [ ! -z "$CMSSW_BASE" ]; then # $CMSSW_BASE is set
+  echo ">>> Setting CMSSW environment from CMSSW_BASE=$CMSSW_BASE..."
+  if [ -d "/cvmfs/cms.cern.ch/" ]; then # /cvmfs exists/mounted
+    peval "source /cvmfs/cms.cern.ch/cmsset_default.sh"
+  else # could not find /cvmfs
+    echo ">>> WARNING! /cvmfs/cms.cern.ch/ does not exist or not mounted on machine $(uname -a), host $HOSTNAME !"
+  fi
+  if [ -d "$CMSSW_BASE/src" ]; then # $CMSSW_BASE exists/mounted
+    peval "cd $CMSSW_BASE/src"
+    peval 'eval $(scramv1 runtime -sh)' # = cmsenv
+    peval "cd $WORKDIR"
+  else # could not find CMSSW
+    echo ">>> WARNING! $CMSSW_BASE/src does not exist or not mounted on machine $(uname -a), host $HOSTNAME !"
+  fi
 fi
 
 # MAIN FUNCTIONALITY
-#TASKCMD=$(cat $JOBLIST | sed "${TASKID}q;d")
+pbar
+#TASKCMD=$(cat $JOBLIST | sed "${TASKID}q;d") # get TASKCMD from job list file
 echo "\$PWD=$PWD"
 peval "$TASKCMD"
 
 # FINISH
 echo
-END=`date +%s`; RUNTIME=$((END-START))
-echo "Job complete at `date`"
+pbar
+peval "rm $WORKDIR/setenv.sh"
+END="$(date +%s)"; RUNTIME=$((END-START))
+echo "Job complete at $(date)"
 printf "Took %d minutes %d seconds" "$(( $RUNTIME / 60 ))" "$(( $RUNTIME % 60 ))"
