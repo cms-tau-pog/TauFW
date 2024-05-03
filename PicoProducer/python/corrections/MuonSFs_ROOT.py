b41afdcd34a5b774e846ea7a4e427ad78020f495
IzaakWN <izaak.neutelings@cern.ch>
2024-05-03 14:10:48 +0200

    add correctionlib JSONs for muon SFs

---
+++ PicoProducer/python/corrections/MuonSFs_ROOT.py
@@ -0,0 +1,100 @@
+# Author: Izaak Neutelings (December 2018)
+# HTT: https://github.com/CMS-HTT/LeptonEfficiencies
+# MuonPOG: https://gitlab.cern.ch/cms-muonPOG/muonefficiencies/-/tree/master/Run2
+# https://twiki.cern.ch/twiki/bin/view/CMS/MuonPOG#User_Recommendations
+# https://twiki.cern.ch/twiki/bin/view/CMS/MuonLegacy2016
+import os
+from TauFW.PicoProducer import datadir
+from TauFW.PicoProducer.corrections.ScaleFactorTool import ScaleFactor, ScaleFactorHTT 
+pathPOG = os.path.join(datadir,"lepton/MuonPOG/")
+pathHTT = os.path.join(datadir,"lepton/HTT/Muon/")
+
+
+class MuonSFs:
+  
+  def __init__(self, era='2017', flags=[ ], verb=0):
+    """Load histograms from files."""
+    
+    #eras = ['2016','2017','2018','UL2017']
+    #assert era in eras, "MuonSFs: You must choose a year from: %s."%(', '.join(eras))
+    self.sftool_trig = None
+    self.sftool_idiso = None
+    if 'UL' in era:
+      if '2016' in era and 'preVFP' in era:
+        # https://twiki.cern.ch/twiki/bin/view/CMS/MuonUL2016
+        self.sftool_trig  = ScaleFactor(pathPOG+"Run2016UL_preVFP/Efficiencies_muon_generalTracks_Z_Run2016_UL_HIPM_SingleMuonTriggers.root",
+                                           'NUM_IsoMu24_or_IsoTkMu24_DEN_CutBasedIdTight_and_PFIsoTight_eta_pt','mu_trig',verb=verb)
+        sftool_id         = ScaleFactor(pathPOG+"Run2016UL_preVFP/Efficiencies_muon_generalTracks_Z_Run2016_UL_HIPM_ID.root","NUM_MediumID_DEN_TrackerMuons_abseta_pt",'mu_id',ptvseta=True,verb=verb)
+        sftool_iso        = ScaleFactor(pathPOG+"Run2016UL_preVFP/Efficiencies_muon_generalTracks_Z_Run2016_UL_HIPM_ISO.root","NUM_TightRelIso_DEN_MediumID_abseta_pt",'mu_iso',ptvseta=True,verb=verb)
+        self.sftool_idiso = sftool_id*sftool_iso
+      elif '2016' in era:
+        # https://twiki.cern.ch/twiki/bin/view/CMS/MuonUL2016
+        self.sftool_trig  = ScaleFactor(pathPOG+"Run2016UL_postVFP/Efficiencies_muon_generalTracks_Z_Run2016_UL_SingleMuonTriggers.root",
+                                           'NUM_IsoMu24_or_IsoTkMu24_DEN_CutBasedIdTight_and_PFIsoTight_eta_pt','mu_trig',verb=verb)
+        sftool_id         = ScaleFactor(pathPOG+"Run2016UL_postVFP/Efficiencies_muon_generalTracks_Z_Run2016_UL_ID.root","NUM_MediumID_DEN_TrackerMuons_abseta_pt",'mu_id',ptvseta=True,verb=verb)
+        sftool_iso        = ScaleFactor(pathPOG+"Run2016UL_postVFP/Efficiencies_muon_generalTracks_Z_Run2016_UL_ISO.root","NUM_TightRelIso_DEN_MediumID_abseta_pt",'mu_iso',ptvseta=True,verb=verb)
+        self.sftool_idiso = sftool_id*sftool_iso
+      elif '2017' in era:
+        # https://twiki.cern.ch/twiki/bin/view/CMS/MuonUL2017
+        if 'HLT_IsoMu27' in flags: # use only HLT_IsoMu27
+          self.sftool_trig = ScaleFactor(pathPOG+"Run2017UL/Efficiencies_muon_generalTracks_Z_Run2017_UL_SingleMuonTriggers.root",
+                                             'NUM_IsoMu27_DEN_CutBasedIdTight_and_PFIsoTight_eta_pt','mu_trig',verb=verb)
+        else: # use HLT_IsoMu24 || HLT_IsoMu27
+          self.sftool_trig  = ScaleFactorHTT(pathHTT+"Run2017/Muon_IsoMu24orIsoMu27.root",'ZMass','mu_trig',verb=verb) # placeholder
+        sftool_id         = ScaleFactor(pathPOG+"Run2017UL/Efficiencies_muon_generalTracks_Z_Run2017_UL_ID.root","NUM_MediumID_DEN_TrackerMuons_abseta_pt",'mu_id',ptvseta=True,verb=verb)
+        sftool_iso        = ScaleFactor(pathPOG+"Run2017UL/Efficiencies_muon_generalTracks_Z_Run2017_UL_ISO.root","NUM_TightRelIso_DEN_MediumID_abseta_pt",'mu_iso',ptvseta=True,verb=verb)
+        self.sftool_idiso = sftool_id*sftool_iso
+      elif '2018' in era:
+        # https://twiki.cern.ch/twiki/bin/view/CMS/MuonUL2017
+        ###self.sftool_trig  = ScaleFactorHTT(pathHTT+"Run2018/Muon_Run2018_IsoMu24orIsoMu27.root",'ZMass','mu_trig',verb=verb) # placeholder
+        self.sftool_trig  = ScaleFactor(pathPOG+"Run2018UL/Efficiencies_muon_generalTracks_Z_Run2018_UL_SingleMuonTriggers.root",
+                                           'NUM_IsoMu24_DEN_CutBasedIdTight_and_PFIsoTight_eta_pt','mu_trig',verb=verb)
+        sftool_id         = ScaleFactor(pathPOG+"Run2018UL/Efficiencies_muon_generalTracks_Z_Run2018_UL_ID.root","NUM_MediumID_DEN_TrackerMuons_abseta_pt",'mu_id',ptvseta=True,verb=verb)
+        sftool_iso        = ScaleFactor(pathPOG+"Run2018UL/Efficiencies_muon_generalTracks_Z_Run2018_UL_ISO.root","NUM_TightRelIso_DEN_MediumID_abseta_pt",'mu_iso',ptvseta=True,verb=verb)
+        self.sftool_idiso = sftool_id*sftool_iso
+    else: # pre-UL
+      if era=='2016':
+        # https://gitlab.cern.ch/cms-muonPOG/MuonReferenceEfficiencies/-/tree/master/EfficienciesStudies/2016_trigger
+        #self.sftool_trig  = ScaleFactorHTT(pathHTT+"Run2016_legacy/Muon_Run2016_legacy_IsoMu22.root",'ZMass','mu_trig',verb=verb)
+        self.sftool_trig  = ScaleFactorHTT(pathHTT+"Run2016_legacy/Muon_Run2016_legacy_IsoMu24.root",'ZMass','mu_trig',verb=verb)
+        self.sftool_idiso = ScaleFactorHTT(pathHTT+"Run2016_legacy/Muon_Run2016_legacy_IdIso.root",'ZMass','mu_idiso',verb=verb)
+        ###self.sftool_trig  = ScaleFactorHTT(pathHTT+"Run2017/Muon_IsoMu24orIsoMu27.root",'ZMass','mu_idiso',verb=verb) # placeholder
+        ###sftool_id         = ScaleFactor(pathPOG+"Run2017UL/Efficiencies_muon_generalTracks_Z_Run2017_UL_ID.root","NUM_MediumID_DEN_TrackerMuons_abseta_pt",'mu_id',ptvseta=True,verb=verb)
+        ###sftool_iso        = ScaleFactor(pathPOG+"Run2017UL/Efficiencies_muon_generalTracks_Z_Run2017_UL_ISO.root","NUM_TightRelIso_DEN_MediumID_abseta_pt",'mu_iso',ptvseta=False,verb=verb)
+        ###self.sftool_idiso = sftool_id*sftool_iso
+      elif era=='2017':
+        #self.sftool_trig  = ScaleFactor(pathPOG+"Run2017/EfficienciesAndSF_RunBtoF_Nov17Nov2017.root","IsoMu27_PtEtaBins/abseta_pt_ratio",'mu_trig')
+        self.sftool_trig  = ScaleFactorHTT(pathHTT+"Run2017/Muon_IsoMu24orIsoMu27.root",'ZMass','mu_idiso',verb=verb)
+        self.sftool_idiso = ScaleFactorHTT(pathHTT+"Run2017/Muon_IdIso_IsoLt0.15_eff_RerecoFall17.root",'ZMass','mu_idiso',verb=verb)
+        ###sftool_id         = ScaleFactor(pathPOG+"Run2017/RunBCDEF_SF_ID.root","NUM_MediumID_DEN_genTracks_pt_abseta",'mu_id',ptvseta=False)
+        ###sftool_iso        = ScaleFactor(pathPOG+"Run2017/RunBCDEF_SF_ISO.root","NUM_TightRelIso_DEN_MediumID_pt_abseta",'mu_iso',ptvseta=False)
+        ###self.sftool_idiso = sftool_id*sftool_iso
+      elif era=='2018':
+        self.sftool_trig  = ScaleFactorHTT(pathHTT+"Run2018/Muon_Run2018_IsoMu24orIsoMu27.root",'ZMass','mu_trig',verb=verb)
+        self.sftool_idiso = ScaleFactorHTT(pathHTT+"Run2018/Muon_Run2018_IdIso.root") # MediumID, DB corrected iso (dR<0.4) < 0.15
+        ###sftool_id         = ScaleFactor(pathPOG+"Run2018/RunABCD_SF_ID.root","NUM_MediumID_DEN_genTracks_pt_abseta",'mu_id',ptvseta=False)
+        ###sftool_iso        = ScaleFactor(pathPOG+"Run2018/RunABCD_SF_ISO.root","NUM_TightRelIso_DEN_MediumID_pt_abseta",'mu_iso',ptvseta=False)
+        ###self.sftool_idiso = sftool_id*sftool_iso
+      elif '2022_preEE' in era:
+        self.sftool_trig  = ScaleFactor(pathPOG+"Run2022preEE/muon_SFs_2022_preEE.root",'ScaleFactor_trg','mu_trig',ptvseta=False,verb=verb)
+        self.sftool_id         = ScaleFactor(pathPOG+"Run2022preEE/muon_SFs_2022_preEE.root","ScaleFactor_id",'mu_id',ptvseta=False)
+        self.sftool_iso        = ScaleFactor(pathPOG+"Run2022preEE/muon_SFs_2022_preEE.root","ScaleFactor_iso",'mu_iso',ptvseta=False)
+        assert self.sftool_id != None and self.sftool_iso != None, "MuonSFs.__init__: Did not find muon ID/ISO tool for %r"%(era)
+        self.sftool_idiso = self.sftool_id*self.sftool_iso
+      elif '2022_postEE' in era:
+        self.sftool_trig  = ScaleFactor(pathPOG+"Run2022postEE/muon_SFs_2022_postEE.root",'ScaleFactor_trg','mu_trig',ptvseta=False,verb=verb)
+        self.sftool_id         = ScaleFactor(pathPOG+"Run2022postEE/muon_SFs_2022_postEE.root","ScaleFactor_id",'mu_id',ptvseta=False)
+        self.sftool_iso        = ScaleFactor(pathPOG+"Run2022postEE/muon_SFs_2022_postEE.root","ScaleFactor_iso",'mu_iso',ptvseta=False)
+        assert self.sftool_id != None and self.sftool_iso != None, "MuonSFs.__init__: Did not find muon ID/ISO tool for %r"%(era)
+        self.sftool_idiso = self.sftool_id*self.sftool_iso
+    assert self.sftool_trig!=None and self.sftool_idiso!=None, "MuonSFs.__init__: Did not find muon SF tool for %r"%(era)
+    print("Loading MuonSF for %s, %s"%(self.sftool_trig.filename,self.sftool_idiso.filename))
+  
+  def getTriggerSF(self, pt, eta):
+    """Get SF for single muon trigger."""
+    return self.sftool_trig.getSF(pt,abs(eta))
+  
+  def getIdIsoSF(self, pt, eta):
+    """Get SF for muon identification + isolation."""
+    return self.sftool_idiso.getSF(pt,abs(eta))
+  
--- PicoProducer/test/testSFs.py
+++ PicoProducer/test/testSFs.py
@@ -2,116 +2,103 @@
 # Author: Izaak Neutelings (December 2018)
 import time
 start0 = time.time()
-print('')
-print(">>> importing modules...")
-from TauFW.common.tools.log import Logger
+print(">>> Importing modules...")
+from TauFW.common.tools.log import Logger, color, underline
 LOG = Logger("testSF")
-
-start1 = time.time()
-from ROOT import TFile
-print(">>>   imported ROOT classes after %.1f seconds"%(time.time()-start1))
-
-start1 = time.time()
-from TauFW.PicoProducer.corrections.ScaleFactorTool import *
-print(">>>   imported ScaleFactorTool classes after %.1f seconds"%(time.time()-start1))
-
-start1 = time.time()
-from TauFW.PicoProducer.corrections.MuonSFs import *
-print(">>>   imported MuonSFs classes after %.1f seconds"%(time.time()-start1))
-
-start1 = time.time()
-from TauFW.PicoProducer.corrections.ElectronSFs import *
-print(">>>   imported ElectronSFs classes after %.1f seconds"%(time.time()-start1))
-
-start1 = time.time()
-from TauFW.PicoProducer.corrections.TauTriggerSFs import *
-print(">>>   imported TauTriggerSFs classes after %.1f seconds"%(time.time()-start1))
-
-start1 = time.time()
-from TauFW.PicoProducer.corrections.BTagTool import *
-print(">>>   imported BTagTool classes after %.1f seconds"%(time.time()-start1))
-
-start1 = time.time()
-from TauFW.PicoProducer.corrections.PileupTool import *
-print(">>>   imported PileupTool classes after %.1f seconds"%(time.time()-start1))
-print(">>>   imported everything after %.1f seconds"%(time.time()-start0))
-print(">>> ")
 
 # PATHS
 path       = 'data/lepton/'
 pathHTT_mu = 'data/lepton/HTT/Muon/Run2017/'
 pathHTT_el = 'data/lepton/HTT/Electron/Run2017/'
+maxerr     = 10 # maximum number of errors
 
 # MATRIX
-def printtable(name,method,ptvals=None,etavals=None):
+def printtable(name,method,ptvals=None,etavals=None,etamax=3.0):
   start2 = time.time()
   if ptvals==None:
-    ptvals  = [ 10., 20., 21., 22., 24., 26., 27., 34., 35., 36., 40., 60., 156., 223., 410., 560. ]
+    ptvals  = [ 10., 20., 21., 22., 24., 25., 26., 27., 34., 35., 36., 40., 60., 156., 223., 410., 560. ]
   if etavals==None:
     etavals = [ 0.0, 0.5, 1.1, 1.9, 2.3, 2.4, 2.8, 3.4 ]
     etavals = [-eta for eta in reversed(etavals)]+etavals
+  if etamax!=None:
+    etavals = [eta for eta in etavals if abs(eta)<etamax]
   print(">>>   %s:"%name)
-  TAB = LOG.table("%9.2f"+" %9.2f"*len(etavals)+"  ")
-  #print(">>>    %10s"%('pt\eta')+' '.join('%10.2f'%eta for eta in etavals))
-  TAB.printheader("pt\eta",*[str(eta) for eta in etavals])
+  #TAB = LOG.table("%9.2f"+" %9.2f"*len(etavals)+"  ")
+  #TAB.printheader("pt\eta",*[str(eta) for eta in etavals])
+  print(">>>   "+underline("%10s "%('pt\eta')+' '.join('%9.2f'%eta for eta in etavals)))
+  errors = [ ]
   for pt in ptvals:
     #print(">>>    %10.2f"%(pt)+' '.join('%10.3f'%method(pt,eta) for eta in etavals))
-    TAB.printrow(pt,*[method(pt,eta) for eta in etavals])
-  print(">>>   got %d SFs in %.3f seconds"%(len(ptvals)*len(etavals),time.time()-start2))
+    #TAB.printrow(pt,*[method(pt,eta) for eta in etavals])
+    row = ">>>   %10.2f"%(pt)
+    for eta in etavals:
+      try:
+        row += " %9.2f"%(method(pt,eta))
+      except Exception as error:
+        row += color("     ERROR",'red')
+        errors.append(error)
+    print(row)
+  print(">>>   Got %d SFs in %.3f seconds"%(len(ptvals)*len(etavals),time.time()-start2))
+  if len(errors)>=1:
+    print(">>>   Caught %s error(s):"%(len(errors)))
+    if maxerr>=1:
+      errors = errors[:maxerr]
+    for error in errors:
+      print(">>>     "+color("%s: %r"%(error.__class__.__name__,str(error)),'red'))
   print(">>> ")
   
 
 def muonPOG():
-  LOG.header("muonPOG")
+  LOG.header("muonPOG (ROOT)")
   
   # TRIGGER (Muon POG)
   start1 = time.time()
-  print(">>> initializing trigger SFs from Muon POG...")
+  print(">>> Initializing trigger SFs from Muon POG (ROOT)...")
   sftool_trig = ScaleFactor(path+"MuonPOG/Run2017/EfficienciesAndSF_RunBtoF_Nov17Nov2017.root","IsoMu27_PtEtaBins/abseta_pt_ratio",'mu_trig',ptvseta=True)
-  print(">>>   initialized in %.1f seconds"%(time.time()-start1))
+  print(">>>   Initialized in %.1f seconds"%(time.time()-start1))
   printtable('trigger POG',sftool_trig.getSF)
   
   # ID (Muon POG)
   start1 = time.time()
   sftool_id  = ScaleFactor(path+"MuonPOG/Run2018/RunABCD_SF_ID.root","NUM_MediumID_DEN_genTracks_pt_abseta",'mu_id',ptvseta=False)
-  print(">>>   initialized in %.1f seconds"%(time.time()-start1))
+  print(">>>   Initialized in %.1f seconds"%(time.time()-start1))
   printtable('id POG',sftool_id.getSF)
   
   # ISO (Muon POG)
   start1 = time.time()
   sftool_iso = ScaleFactor(path+"MuonPOG/Run2018/RunABCD_SF_ISO.root","NUM_TightRelIso_DEN_MediumID_pt_abseta",'mu_iso',ptvseta=False)
-  print(">>>   initialized in %.1f seconds"%(time.time()-start1))
+  print(">>>   Initialized in %.1f seconds"%(time.time()-start1))
   printtable('iso POG',sftool_iso.getSF)
   
   # ID/ISO (Muon POG)
   start1 = time.time()
   sftool_idiso    = sftool_id*sftool_iso
-  print(">>>   initialized in %.1f seconds"%(time.time()-start1))
+  print(">>>   Initialized in %.1f seconds"%(time.time()-start1))
   printtable('idiso POG',sftool_idiso.getSF)  
   print(">>> ")
   
 
 def muonHTT():
-  LOG.header("muonHTT")
+  LOG.header("muonHTT (ROOT)")
   
   # TRIGGER (HTT)
   start1 = time.time()
-  print(">>> initializing trigger SFs from HTT...")
+  print(">>> Initializing trigger SFs from HTT...")
   sftool_mu_trig_HTT = ScaleFactorHTT(pathHTT_mu+"Muon_IsoMu24orIsoMu27.root","ZMass",'mu_idiso')
-  print(">>>   initialized in %.1f seconds"%(time.time()-start1))
+  print(">>>   Initialized in %.1f seconds"%(time.time()-start1))
   printtable('trigger HTT',sftool_mu_trig_HTT.getSF)
   
   ## ID ISO (HTT)
   #start1 = time.time()
-  #print(">>> initializing idiso SFs from HTT...")
+  #print(">>> Initializing idiso SFs from HTT...")
   #sftool_mu_idiso_HTT = ScaleFactorHTT(pathHTT_mu+"Muon_IdIso_IsoLt0p15_eff_RerecoFall17.root","ZMass",'mu_idiso')
-  #print(">>>   initialized in %.1f seconds"%(time.time()-start1))
+  #print(">>>   Initialized in %.1f seconds"%(time.time()-start1))
   #printtable('idiso HTT',sftool_mu_idiso_HTT.getSF)
   #print(">>> ")
   
 
 def electronHTT():
-  LOG.header("electronHTT")
+  LOG.header("electronHTT (ROOT)")
   
   # RECO (EGAMMA POG)
   sftool_ele_reco_HTT = ScaleFactor(pathHTT_el+"egammaEffi.txt_EGM2D_runBCDEF_passingRECO.root","EGamma_SF2D",'ele_reco',ptvseta=True)
@@ -122,25 +109,14 @@ def electronHTT():
   printtable('idiso HTT',sftool_ele_idiso_HTT.getSF)
   
 
-def muonSFs():
-  LOG.header("muonSFs")
+def muonSFs_JSON(era='UL2018'):
+  LOG.header("muonSFs (JSON)")
   
   # MUON SFs
   start1 = time.time()
-  print(">>> initializing MuonSF object...")
-  muSFs = MuonSFs()
-  print(">>>   initialized in %.1f seconds"%(time.time()-start1))
-  
-  # GET SFs
-  printtable('trigger',muSFs.getTriggerSF)
-  printtable('idiso',muSFs.getIdIsoSF)
-  print(">>> ")
-  
-  # MUON 2018 SFs
-  start1 = time.time()
-  print(">>> initializing MuonSF(2018) object...")
-  muSFs = MuonSFs(year=2018)
-  print(">>>   initialized in %.1f seconds"%(time.time()-start1))
+  print(">>> Initializing MuonSF object for era=%r..."%(era))
+  muSFs = MuonSFs(era)
+  print(">>>   Initialized in %.1f seconds"%(time.time()-start1))
   
   # GET SFs
   printtable('trigger',muSFs.getTriggerSF)
@@ -148,15 +124,30 @@ def muonSFs():
   print(">>> ")
   
 
-def electronSFs():
+def muonSFs_ROOT(era='UL2018'):
+  LOG.header("muonSFs (ROOT)")
+  
+  # MUON SFs
+  start1 = time.time()
+  print(">>> Initializing MuonSF object for era=%r..."%(era))
+  muSFs = MuonSFs_ROOT(era)
+  print(">>>   Initialized in %.1f seconds"%(time.time()-start1))
+  
+  # GET SFs
+  printtable('trigger',muSFs.getTriggerSF)
+  printtable('idiso',muSFs.getIdIsoSF)
+  print(">>> ")
+  
+
+def electronSFs(era='UL2019'):
   LOG.header("electronSFs")
   
   # ELECTRON SFs
   print(">>> ")
   start1 = time.time()
-  print(">>> initializing ElectronSFs object...")
-  eleSFs = ElectronSFs()
-  print(">>>   initialized in %.1f seconds"%(time.time()-start1))
+  print(">>> Initializing ElectronSFs object for era=%s..."%(era))
+  eleSFs = ElectronSFs(era)
+  print(">>>   Initialized in %.1f seconds"%(time.time()-start1))
   
   # GET SFs
   printtable('trigger',eleSFs.getTriggerSF)
@@ -177,34 +168,34 @@ def tauTriggerSFs():
   printtable('trigger fake',lambda p,d: tauSFs.getSF(p,d),ptvals=ptvals,etavals=dmvals)
   
 
-def btagSFs(tagger='CSVv2'):
+def btagSFs(tagger='DeepCSV'):
   LOG.header("btagSFs")
   
   # BTAG SFs
   print(">>> ")
   start1 = time.time()
-  print(">>> initializing BTagWeightTool(%r) object..."%tagger)
+  print(">>> Initializing BTagWeightTool(%r) object..."%tagger)
   btagSFs = BTagWeightTool(tagger)
-  print(">>>   initialized in %.1f seconds"%(time.time()-start1))
+  print(">>>   Initialized in %.1f seconds"%(time.time()-start1))
   
   # GET SFs
-  printtable('%s g'%tagger,lambda p,e: btagSFs.getSF(p,e,0,True))
-  #printtable('%s u'%tagger,lambda p,e: btagSFs.getSF(p,e,1,True))
-  #printtable('%s d'%tagger,lambda p,e: btagSFs.getSF(p,e,2,True))
-  #printtable('%s s'%tagger,lambda p,e: btagSFs.getSF(p,e,3,True))
-  printtable('%s c'%tagger,lambda p,e: btagSFs.getSF(p,e,4,True))
-  printtable('%s b'%tagger,lambda p,e: btagSFs.getSF(p,e,5,True))
+  printtable('%s g'%tagger,lambda p,e: btagSFs.getSF(p,e,0,True),etamax=5)
+  #printtable('%s u'%tagger,lambda p,e: btagSFs.getSF(p,e,1,True),etamax=5)
+  #printtable('%s d'%tagger,lambda p,e: btagSFs.getSF(p,e,2,True),etamax=5)
+  #printtable('%s s'%tagger,lambda p,e: btagSFs.getSF(p,e,3,True),etamax=5)
+  printtable('%s c'%tagger,lambda p,e: btagSFs.getSF(p,e,4,True),etamax=5)
+  printtable('%s b'%tagger,lambda p,e: btagSFs.getSF(p,e,5,True),etamax=5)
   
 
-def pileupSFs(era='UL2017'):
+def pileupSFs(era='UL2018'):
   LOG.header("pileupSFs")
   
   # PILE UP TOOL
   print(">>> ")
   start1 = time.time()
-  print(">>> initializing PileupTool(%r) object..."%era)
+  print(">>> Initializing PileupTool(%r) object..."%era)
   puTool = PileupWeightTool(era)
-  print(">>>   initialized in %.1f seconds"%(time.time()-start1))
+  print(">>>   Initialized in %.1f seconds"%(time.time()-start1))
   
   ## GET SFs
   start2 = time.time()
@@ -213,21 +204,108 @@ def pileupSFs(era='UL2017'):
   npus = [0,1,2,5,10,15,18,20,24,25,26,28,30,35,40,50,60,70,80,100]
   for npu in npus:
     TAB.printrow(npu,puTool.getWeight(npu))
-  print(">>>   got %d SFs in %.3f seconds"%(len(npus),time.time()-start2))
+  print(">>>   Got %d SFs in %.3f seconds"%(len(npus),time.time()-start2))
+  
+
+def main(args):
+  tools = args.tools
+  eras  = args.eras
+  
+  # LEPTON SF
+  if not tools or 'musf' in tools:
+    muonPOG()
+    muonHTT()
+  if not tools or 'elesf' in tools:
+    electronHTT()
+  if not tools or 'mu' in tools:
+    for era in eras:
+      muonSFs_JSON(era)
+    for era in eras:
+      muonSFs_ROOT(era)
+  if not tools or 'ele' in tools:
+    electronSFs()
+  
+  # TAU
+  if not tools or 'tau' in tools:
+    tauTriggerSFs()
+  
+  # BTV
+  if not tools or 'btag' in tools:
+    #btagSFs('CSVv2')
+    btagSFs('DeepCSV')
+  
+  # PU
+  if not tools or 'pu' in tools:
+    for era in eras:
+      pileupSFs(era)
   
 
 if __name__ == "__main__":
+  from argparse import ArgumentParser
+  description = """Test corrections tools"""
+  parser = ArgumentParser(description=description,epilog="Good luck!")
+  parser.add_argument('-t','--tool',    dest='tools', nargs='+',
+                                        help="corrections tools to test" )
+  parser.add_argument('-y','--era',     dest='eras', nargs='+', default=['UL2018'],
+                                        help="eras to run" )
+  parser.add_argument('-m','--maxerr',  dest='maxerr', type=int, default=10,
+                                        help="maximum nubmer of errors to print" )
+  parser.add_argument('-v','--verbose', dest='verbosity', type=int, nargs='?', const=1, default=0, action='store',
+                                        help="set verbosity level" )
+  args   = parser.parse_args()
+  tools  = args.tools
+  maxerr = args.maxerr
   
-  muonPOG()
-  #muonHTT()
-  #electronHTT()
-  #muonSFs()
-  electronSFs()
-  tauTriggerSFs()
-  #btagSFs('CSVv2')
-  btagSFs('DeepCSV')
-  pileupSFs('2017')
-  pileupSFs('UL2017')
+  # IMPORTS
+  start1 = time.time()
+  from ROOT import TFile # typically slows down other modules
+  print(">>>   Imported ROOT classes after %.1f seconds"%(time.time()-start1))
+  
+  if any(s in tools for s in ['musf','elesf']):
+    start1 = time.time()
+    from TauFW.PicoProducer.corrections.ScaleFactorTool import *
+    print(">>>   Imported ScaleFactorTool classes after %.1f seconds"%(time.time()-start1))
+  
+  if 'mu' in tools:
+    start1 = time.time()
+    from TauFW.PicoProducer.corrections.MuonSFs import *
+    print(">>>   Imported MuonSFs classes after %.1f seconds"%(time.time()-start1))
+  
+  if 'mu' in tools:
+    start1 = time.time()
+    from TauFW.PicoProducer.corrections.MuonSFs_ROOT import MuonSFs as MuonSFs_ROOT
+    print(">>>   Imported MuonSFs_ROOT classes after %.1f seconds"%(time.time()-start1))
+  
+  if 'ele' in tools:
+    start1 = time.time()
+    from TauFW.PicoProducer.corrections.ElectronSFs import *
+    print(">>>   Imported ElectronSFs classes after %.1f seconds"%(time.time()-start1))
+  
+  if 'tau' in tools:
+    start1 = time.time()
+    from TauFW.PicoProducer.corrections.TauTriggerSFs import *
+    print(">>>   Imported TauTriggerSFs classes after %.1f seconds"%(time.time()-start1))
+  
+  if 'btag' in tools:
+    start1 = time.time()
+    from TauFW.PicoProducer.corrections.BTagTool import *
+    print(">>>   Imported BTagTool classes after %.1f seconds"%(time.time()-start1))
+  
+  if 'pu' in tools:
+    start1 = time.time()
+    from TauFW.PicoProducer.corrections.PileupTool import *
+    print(">>>   Imported PileupTool classes after %.1f seconds"%(time.time()-start1))
+  
+  if 'jetveto' in tools:
+    start1 = time.time()
+    from TauFW.PicoProducer.corrections.JetVetoMapTool import *
+    print(">>>   Imported JetVetoMapTool classes after %.1f seconds"%(time.time()-start1))
+    
+  print(">>>   Imported everything after %.1f seconds"%(time.time()-start0))
   print(">>> ")
-  print(">>> done after %.1f seconds"%(time.time()-start0))
+  
+  # MAIN FUNCTIONALITY
+  main(args)
+  print(">>> ")
+  print(">>> Done after %.1f seconds"%(time.time()-start0))
   print('')
