75f9dc2d95ff28aab941409d1a45257d78a30481
IzaakWN <iwn_@hotmail.com>
2024-06-14 14:53:09 +0200

    I am an idiot, apologies Paola!

--- PicoProducer/python/__init__.py
+++ PicoProducer/python/__init__.py
@@ -1,7 +1,7 @@
 # Author: Izaak Neutelings (May 2020)
 import os
-if 'CMSSW_BASE' in os.environ: # look for $CMSSW_BASE/src/TauFW
-  basedir = os.path.join(os.environ['CMSSW_BASE'],"src/TauFW")
+if 'CMSSW_BASE' in os.environ: # look for $CMSSW_BASE/src/TauFW/PicoProducer
+  basedir = os.path.join(os.environ['CMSSW_BASE'],"src/TauFW/PicoProducer")
 else: # expand symlink
   basedir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 assert os.path.exists(basedir), f"Could not parse PicoProducer's basedir={basedir}"
