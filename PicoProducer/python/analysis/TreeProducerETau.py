b863040b0e4ad4dc85279f562ea849d3a2d14483
Francisco Casalinho <francisco.casalinho@cern.ch>
2025-05-22 08:12:53 +0000

    add the UParT taggers for the etau channel

--- PicoProducer/python/analysis/TreeProducerETau.py
+++ PicoProducer/python/analysis/TreeProducerETau.py
@@ -90,6 +90,19 @@ class TreeProducerETau(TreeProducerTauPair):
     self.addBranch('photonsOutsideSignalCone_2', 'f')
     self.addBranch('puCorr_2',                   'f')
     self.addBranch('jpt_match_2',                'f', -1, title="pt of jet matching tau")
+  
+
+    self.addBranch('decayModeUParT_2',              'i')
+    self.addBranch('rawUParTVSe_2',                   'f')
+    self.addBranch('rawUParTVSmu_2',                   'f')
+    self.addBranch('rawUParTVSjet_2',                   'f')
+    self.addBranch('probDM0UParT_2',               'f')
+    self.addBranch('probDM1UParT_2',               'f')
+    self.addBranch('probDM2UParT_2',               'f')
+    self.addBranch('probDM10UParT_2',               'f')
+    self.addBranch('probDM11UParT_2',               'f')
+    self.addBranch('ptCorrUParT_2',                'f')
+    self.addBranch('qConfUParT_2',                'f')
     
     if self.module.ismc:
       self.addBranch('jpt_genmatch_2',           'f', -1, title="pt of gen jet matching tau")
